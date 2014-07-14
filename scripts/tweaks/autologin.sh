61e139f9102ad79efb7af3528288f4e79db0cb78
jmcerrejon <ulysess@gmail.com>
2014-07-14 17:41:35 +0200

    Added autologin for Ubuntu users (Don't tell this to anyone)

--- scripts/tweaks/autologin.sh
+++ scripts/tweaks/autologin.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #
-# Description : Autologin for Raspbian
+# Description : Autologin for Raspbian/Ubuntu
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0 (14/May/14)
+# Version     : 1.1 (14/Jul/14)
 #
 #
 clear
 
-# Add comment to 1:2345:respawn:/sbin/getty...
-sudo sed -i '/1:2345/s/^/#/' /etc/inittab
+OS=$(lsb_release -si)
 
-# Insert new file on pattern position
-sudo sed -i 's/.*1:2345.*/&\n1:2345:respawn:\/bin\/login -f pi tty1 <\/dev\/tty1> \/dev\/tty1 2>\&1/' /etc/inittab
+if [[ $OS=='Ubuntu' ]]; then
+	# Add comment to 1:2345:respawn:/sbin/getty...
+	sudo sed -i '/tty1/s/^/#/' /etc/init/tty1.conf
 
+	# Insert new file on pattern position
+	sudo sed -i '$i exec \/bin\/login -f '$USER' < \/dev\/tty1 > \/dev\/tty1 2>&1' /etc/init/tty1.conf
+else
+	# Add comment to 1:2345:respawn:/sbin/getty...
+	sudo sed -i '/1:2345/s/^/#/' /etc/inittab
+
+	# Insert new file on pattern position
+	sudo sed -i 's/.*tty1.*/&\n1:2345:respawn:\/bin\/login -f '$USER' tty1 <\/dev\/tty1> \/dev\/tty1 2>\&1/' /etc/inittab
+fi
 read -p "Done!. Now your distro have free access and no need to login on boot!. Press [Enter] to continue..."
