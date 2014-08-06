8e834e0af8bf9a093c072e40b3b7445a4783d8e9
jmcerrejon <ulysess@gmail.com>
2014-08-06 12:14:14 +0200

    Script fixed!

--- scripts/others/aircrack.sh
+++ scripts/others/aircrack.sh
@@ -11,6 +11,6 @@ clear
 echo -e "Compile & Install Aircrack-NG / Airoscript\n============================================\n"
 
 cd $HOME
-wget -o https://raw.github.com/txt3rob/Aircrack-NG_RaspberryPI/master/install.sh
+wget https://raw.githubusercontent.com/txt3rob/Aircrack-NG_RaspberryPI/master/install.sh
 sudo chmod 777 install.sh
 sudo ./install.sh
