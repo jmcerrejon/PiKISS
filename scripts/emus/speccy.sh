a778b5928d44758416efdb9c300bda170da10e47
jmcerrejon <ulysess@gmail.com>
2014-09-29 13:26:04 +0200

    New Spectrum emulator with Joystick support thanks to jfroco

--- scripts/emus/speccy.sh
+++ scripts/emus/speccy.sh
@@ -2,12 +2,12 @@
 #
 # Description : Portable ZX-Spectrum emulator
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1 (17/Jun/14)
+# Version     : 1.2 (29/Sep/14)
 #
 clear
 
 INSTALL_DIR="/home/$USER/games/"
-URL_FILE="https://bitbucket.org/djdron/unrealspeccyp/downloads/unreal-speccy-portable_0.0.43_rpi.zip"
+URL_FILE="https://dl.dropboxusercontent.com/u/4281970/pi/usp_0.0.59_jfroco.zip"
 
 validate_url(){
     if [[ `wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
@@ -23,7 +23,9 @@ install(){
     # Check if Ok...
         mkdir -p $INSTALL_DIR && cd $_
         wget -qO- -O tmp.zip $URL_FILE && unzip -o tmp.zip && rm tmp.zip
-wget -O $INSTALL_DIR/usp_0.0.43/ninjajar.tap http://www.mojontwins.com/juegos/mojon-twins--ninjajar-eng-v1.1.tap
+        cd us*
+        chmod +x unreal_speccy_portable
+wget -O $INSTALL_DIR/usp_0.0.59_jfroco/ninjajar.tap http://www.mojontwins.com/juegos/mojon-twins--ninjajar-eng-v1.1.tap
 echo "Done!. To play go to install path, copy any .tap file to directory and type: ./unreal_speccy_portable <game name>"
 
     read -p "Press [Enter] to continue..."
