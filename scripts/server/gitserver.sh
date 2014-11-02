211ff1311d6858220d8718b27c9570c9a9b8b007
Jose Cerrejon <ulysess@gmail.com>
2014-11-02 20:56:04 +0100

    Added Git Server script

--- scripts/server/gitserver.sh
+++ scripts/server/gitserver.sh
@@ -2,21 +2,24 @@
 #
 # Description : Git Server
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.1 (2/Nov/14)
+# Version     : 1.0 (2/Nov/14)
 #
 # HELP        · http://www.instructables.com/id/GitPi-A-Private-Git-Server-on-Raspberry-Pi/all/?lang=es
 #
 clear
-IP=$(hostname -I)
-read -p "This make a Git Server on $HOME/gitserver. Press [ENTER] to continue or [CTRL]+C to Quit."
+IP=$(/sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+INPUT=/tmp/option.sh.$$
+trap "rm $INPUT; exit" SIGHUP SIGINT SIGTERM
+
+read -p "This make a Git Server on $HOME/gitserver.git/repository_name. Press [ENTER] to continue or [CTRL]+C to Quit."
 clear
-dialog --inputbox "Enter your repo name:" 8 40 2>"${tempfile}"
+dialog --inputbox "Enter your repo name:" 8 40 2>"${INPUT}"
 
-echo $(<"${tempfile}")
+echo $(<"${INPUT}")
 
-#sudo apt-get install -y wget git-core
+mkdir -p $HOME/gitserver.git/$(<"${INPUT}") && cd $_
+git init --bare
 
-#mkdir -p $HOME/gitserver.git && cd $_
-#git init --bare
+# On local repository: git add . && git commit -m "initial commit" && git push pi master
 
-echo -e "Now execute on your local git repository: git remote add pi pi@$IP:$HOME/gitserver.git"
+echo -e "Now execute on your local git repository: git remote add pi pi@$IP:$HOME/gitserver.git/$(<"${INPUT}")\nWhen you want to submit the changes: git push pi master\nNOTE:This does not upload the files, only the changes."
