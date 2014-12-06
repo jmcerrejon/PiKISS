b0b0799c9c90aa5f33f7af6cb8dd38e4c6ee98b8
Jose Cerrejon <ulysess@gmail.com>
2014-12-06 10:25:37 +0100

    Added new binary with 4 players support

--- scripts/emus/pifba.sh
+++ scripts/emus/pifba.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Description : Final Burn Alpha 2x for Raspberry Pi by Squid
+# Description : Final Burn Alpha 2x for Raspberry Pi with 4 players
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.9 (7/May/14)
+# Version     : 1.0 (24/Nov/14)
 #
 clear
 
 INSTALL_DIR="/home/$USER/games/pifba/"
-URL_FILE="http://pifba.googlecode.com/git/piFBA.zip"
+URL_FILE="https://github.com/digitalLumberjack/pifba/releases/download/0.1/pifba-0.1.zip"
 
 validate_url(){
     if [[ `wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
@@ -26,16 +26,16 @@ install(){
     else 
         mkdir -p $INSTALL_DIR && cd $_
         wget -qO- -O tmp.zip $URL_FILE && unzip -o tmp.zip && rm tmp.zip
-        chmod 777 ./fba2x ./fbacapex ./capex.cfg ./fba2x.cfg ./zipname.fba ./rominfo.fba ./FBACache_windows.zip ./fba_029671_clrmame_dat.zip ./roms ./skin ./preview ./preview/*
-        echo "Done!. To play go to install path, copy any rom to /roms directory and type: ./fbacapex to show the front-end"
+        #chmod 777 ./fba2x ./fbacapex ./capex.cfg ./fba2x.cfg ./zipname.fba ./rominfo.fba ./FBACache_windows.zip ./fba_029671_clrmame_dat.zip ./roms ./skin ./preview ./preview/*
+        echo "Done!. To play go to install path, copy any rom to /roms directory and type: ./fba2x ./path/to/rom"
     fi
     read -p "Press [Enter] to continue..."
     exit
 }
 
-echo "Final Burn Alpha 2x for Raspberry Pi"
-echo "===================================="
-echo -e "More Info: https://code.google.com/p/pifba/\n\nInstall path: $INSTALL_DIR"
+echo "Final Burn Alpha 2x for Raspberry Pi (4 players)"
+echo "================================================"
+echo -e "More Info: https://github.com/digitalLumberjack/pifba\n\nInstall path: $INSTALL_DIR"
 while true; do
     echo " "
     read -p "Is it right? [y/n] " yn
