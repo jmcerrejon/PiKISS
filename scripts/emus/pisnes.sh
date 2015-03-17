e7d00d8e435bbaa2d5377feebb3768b2cd919dae
Jose Cerrejon <ulysess@gmail.com>
2015-03-17 17:32:39 +0100

    Updated with a new pisnes & added the homebrew game Uwol

--- scripts/emus/pisnes.sh
+++ scripts/emus/pisnes.sh
@@ -2,16 +2,17 @@
 #
 # Description : PiSNES for Raspberry Pi by Squid
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.9 (15/May/14)
+# Version     : 0.9.1 (17/Mar/15)
+# Compatible  : Raspberry Pi 1 & 2 (tested)
 #
 clear
 
-INSTALL_DIR="/home/$USER/games/pisnes/"
-URL_FILE="http://pisnes.googlecode.com/git/pisnes.zip"
+. ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'http://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-validate_url(){
-    if [[ `wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
-}
+INSTALL_DIR="$HOME/games/pisnes/"
+URL_FILE="http://sourceforge.net/projects/pisnes/files/latest/download?source=files"
+GAME_URL="http://www.dropbox.com/s/b6tl84p3a17w5ab/UwolQuestForMoney.sfc?dl=0"
 
 changeInstallDir(){
     echo "Enter new full path:"
@@ -20,20 +21,16 @@ changeInstallDir(){
 }
 
 install(){
-    if [[ $(validate_url $URL_FILE) != "true" ]] ; then
-        echo "Sorry, the emulator is not available here: $URL_FILE. Visit the website to download it manually."
-        exit
-    else 
-        mkdir -p $INSTALL_DIR && cd $_
-        wget -qO- -O tmp.zip $URL_FILE && unzip -o tmp.zip && rm tmp.zip
-        chmod 777 ./snes9x ./snes9x.cfg ./roms ./skins
-        echo "Done!. To play go to install path, copy any rom to /roms directory and type: ./snes9x <rom name>"
-    fi
+    mkdir -p $INSTALL_DIR && cd $_
+    wget -qO- -O tmp.zip $URL_FILE && unzip -o tmp.zip && rm tmp.zip
+    chmod 777 ./snes9x ./snes9x.cfg ./roms ./skins
+    echo -e "\nInstalling game Uwol Quest For Money (uwol.sfc) on $INSTALL_DIR\n\n" && wget -q -O $INSTALL_DIR/uwol.sfc  $GAME_URL
+    echo "Done!. To play go to install path, copy any rom to /roms directory and type: ./snes9x <rom name>"
     read -p "Press [Enter] to continue..."
     exit
 }
 
-echo -e "Install PiSNES (Version 1.39)\n=============================\nMore Info: http://pisnes.googlecode.com\n\nInstall path: $INSTALL_DIR"
+echo -e "Install PiSNES (Version 1.39)\n=============================\nMore Info: http://sourceforge.net/projects/pisnes\n\nInstall path: $INSTALL_DIR"
 while true; do
     echo " "
     read -p "Is it right? [y/n] " yn
