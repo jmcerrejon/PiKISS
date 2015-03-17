299e274a4b4bd0279450d08f05b0433a57451f79
Jose Cerrejon <ulysess@gmail.com>
2015-03-17 15:32:48 +0100

    Added HIGH texture quality & OGG music

--- scripts/games/descent.sh
+++ scripts/games/descent.sh
@@ -2,7 +2,7 @@
 #
 # Description : Descent 1 & 2
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.1 (17/Mar/15)
+# Version     : 0.3 (17/Mar/15)
 # Compatible  : Raspberry Pi 1 & 2 (tested)
 #   
 # HELP 		  : To uninstall: sudo dpkg -r d1x-rebirth-data-shareware d1x-rebirth d2x-rebirth-data-demo d2x-rebirth
@@ -13,6 +13,10 @@ D1X_SHARE_URL='https://www-user.tu-chemnitz.de/~heinm/dxx/deb/d1x-rebirth-data-s
 D2X_SHARE_URL='https://www-user.tu-chemnitz.de/~heinm/dxx/deb/d2x-rebirth-data-demo_1.0-1_all.deb'
 D1X_URL='https://www-user.tu-chemnitz.de/~heinm/dxx/deb/d1x-rebirth_0.58.1-1_armhf.deb'
 D2X_URL='https://www-user.tu-chemnitz.de/~heinm/dxx/deb/d2x-rebirth_0.58.1-1_armhf.deb'
+D1X_HIGH_TEXTURE_URL='http://www.dxx-rebirth.com/download/dxx/res/d1xr-hires.dxa'
+D1X_OGG_URL='http://www.dxx-rebirth.com/download/dxx/res/d1xr-sc55-music.dxa'
+D2X_OGG_URL='http://www.dxx-rebirth.com/download/dxx/res/d2xr-sc55-music.dxa'
+GAME_DIR='/usr/share/games'
 
 if  which /usr/games/d1x-rebirth >/dev/null ; then
     read -p "Warning!: D1X Rebirth already installed. Press [ENTER] to continue..."
@@ -23,20 +27,25 @@ if  which /usr/games/d2x-rebirth >/dev/null ; then
 fi
 
 D1X_RPI(){
+	clear && echo -e "\nInstalling Descent 1 for Raspberry Pi\n=====================================\n\n· Please wait...\n"
 	wget -P /temp $D1X_SHARE_URL $D1X_URL
 	sudo apt-get install -y libphysfs1
 	sudo dpkg -i /temp/d1x-rebirth_0.58.1-1_armhf.deb /temp/d1x-rebirth-data-shareware_1.4-1_all.deb
 	rm /temp/d1x-rebirth_0.58.1-1_armhf.deb /temp/d1x-rebirth-data-shareware_1.4-1_all.deb
+	clear && echo -e "\nInstalling HIGH textures quality...\n\n· Please wait...\n" && sudo wget -P $GAME_DIR $D1X_HIGH_TEXTURE_URL
+	clear && echo -e "\nInstalling OGG Music for better experience...\n\n·All music was recorded with the Roland Sound Canvas SC-55 MIDI Module.\n· Please wait...\n" && sudo wget -P $GAME_DIR/d1x-rebirth $D1X_OGG_URL
 }
 
 D2X_RPI(){
+	clear && echo -e "\nInstalling Descent 2 for Raspberry Pi\n=====================================\n\n· Please wait...\n"
 	wget -P /temp $D2X_SHARE_URL $D2X_URL
 	sudo apt-get install -y libphysfs1
 	sudo dpkg -i /temp/d2x-rebirth_0.58.1-1_armhf.deb /temp/d2x-rebirth-data-demo_1.0-1_all.deb
 	rm /temp/d2x-rebirth_0.58.1-1_armhf.deb /temp/d2x-rebirth-data-demo_1.0-1_all.deb
+	clear && echo -e "\nInstalling OGG Music for better experience...\n\n·All music was recorded with the Roland Sound Canvas SC-55 MIDI Module.\n· Please wait...\n" && sudo wget -P $GAME_DIR/d2x-rebirth $D2X_OGG_URL
 }
 
-cmd=(dialog --separate-output --title "[ Descent 1 & 2 Shareware ]" --checklist "Move with the arrows up & down. Space to select the game(s) you want to install:" 8 135 16)
+cmd=(dialog --separate-output --title "[ Descent 1 & 2 Shareware ]" --checklist "Move with the arrows up & down. Space to select the game(s) you want to install:" 9 135 16)
 options=(
          Descent_1 "The game requires the player to navigate labyrinthine mines while fighting virus-infected robots." on
          Descent_2 "the player must complete 24 levels where different types of AI-controlled robots will try to destroy you." off)
