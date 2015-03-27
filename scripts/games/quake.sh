60953b3a75e637aa4cf8ef3c470b4370b17b4c6a
Jose Cerrejon <ulysess@gmail.com>
2015-03-27 10:33:29 +0100

    Final commit to ODROID-C1 (I hope). Added Shareware version.

--- scripts/games/quake.sh
+++ scripts/games/quake.sh
@@ -6,12 +6,13 @@
 # Compatible  : Raspberry Pi 1 & 2 (tested), ODROID-C1 (tested)
 # Know bugs   : RPi with the latest Raspbian: black screen | ODROID can't change 640x480 resolution
 #
-# HELP    
-#             · Quake 1: https://github.com/welford/qurp    
+# HELP
+#             · Quake 1: https://github.com/welford/qurp
 #             · QuakeServer: http://www.recantha.co.uk/blog/?p=9962
 #             · Darkplaces Quake: http://www.raspberrypi.org/forums/viewtopic.php?t=72301
 #             · http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=18853
 #             · http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=54683
+#             · http://forums.steampowered.com/forums/showthread.php?t=996272 | http://quake.wikia.com/wiki/Quake_2_Soundtrack
 #
 clear
 
@@ -21,10 +22,12 @@ check_board || { echo "Missing file helper.sh. I've tried to download it for you
 Q1_DEMO_PAK_URL="http://www.quakeforge.net/files/quake-shareware-1.06.zip"
 QUAKE2_RPI_URL="http://pickle.gp2x.de/rpi/quake2_rpi.zip"
 Q2_PAK_URL="http://www.dropbox.com/s/bh5co9nnmy0xf0n/baseq2.zip?dl=0"
+Q2_OGG_URL="https://www.dropbox.com/s/z7c8lm8weemf2iy/q2_ogg.zip?dl=0"
 Q2_CONFIG_URL="https://raw.githubusercontent.com/jmcerrejon/PiKISS/master/res/config.cfg"
 Q3_DEMO_PAK_URL="http://joshua14.homelinux.org/downloads/Q3-Demo-Paks.zip"
 DATA_DIR="$HOME/games/quake2"
 EXEC="$DATA_DIR/quake2"
+LICENSE="Complete"
 
 quake2_ODROID(){
   DATA_DIR="$HOME/.yq2"
@@ -47,15 +50,19 @@ quake2_Raspberry(){
 }
 
 quakeServer(){
-    wget http://downloads.sourceforge.net/project/nquake/nQuakesv%20%28Linux%29/v1.3/nquakesv13_installer.tar.gz
-    tar xzvc nquakesv13_installer.tar.gz
-    cd nquakesv13_installer/
-    ./install_nquakesv.sh
-    # Unfinished...
-    ./run/port1.sh
+  wget http://downloads.sourceforge.net/project/nquake/nQuakesv%20%28Linux%29/v1.3/nquakesv13_installer.tar.gz
+  tar xzvc nquakesv13_installer.tar.gz
+  cd nquakesv13_installer/
+  ./install_nquakesv.sh
+  # Unfinished...
+  ./run/port1.sh
 }
 
-echo -e "Installing Quake 2\n==================\n·Know issues:\n· RPi: Black screen on latest Raspbian.\n· ODROID: 640x480 square box if your resolution is higher.\n\n"
+share_version(){
+  Q2_PAK_URL="https://www.dropbox.com/s/sbr0xwr9wo9been/baseq2s.zip?dl=0"
+}
+
+echo -e "Installing Quake 2\n==================\n\n· OGG soundtrack\n·720p (You can change that)\n·Know issues:\n· RPi: Black screen on latest Raspbian without X.\n\n"
 
 if [[ ${MODEL} == 'Raspberry Pi' ]]; then
   quake2_Raspberry
@@ -63,20 +70,24 @@ elif [[ ${MODEL} == 'ODROID-C1' ]]; then
   quake2_ODROID
 fi
 
-dialog   --title     "[ Download Quake2 .PAK files ]" \
-         --yes-label "Yes" \
-         --no-label  "No" \
-         --yesno     "You need the .PAK files from the original game. Do you want I download it for you (99 MB)? (In some countries the laws may consider it like piracy software)" 7 55
+dialog --title     "[ Quake II. PAK License ]" \
+  --yes-label "Shareware" \
+  --no-label  "Complete" \
+  --yesno     "Choose what type of PAK files do you want to install. NOTE: For complete version, you must be the owner of the original game (in some countries)" 7 55
 
 retval=$?
 
 case $retval in
-  0)   echo -e "Installing...\n" ;;
-  1)   clear ; read -p "Please copy into $DATA_DIR/baseq2 all the .pak files from the original game and run with: $EXEC. Press [Enter] to continue..." ; exit ;;
+  0)   share_version ; LICENSE="Shareware"; 
+  255) exit ;;
 esac
-#wget -P $HOME/.yq2 -O baseq2.zip http://www.dropbox.com/s/bh5co9nnmy0xf0n/baseq2.zip?dl=0 && unzip $HOME/baseq2.zip -d $HOME/.yq2
-wget -P $DATA_DIR -O baseq2.zip $Q2_PAK_URL
-unzip $HOME/baseq2.zip -d $DATA_DIR
-rm $HOME/baseq2.zip
 
-read -p "Press [Enter] to continue..."
+echo -e "\n\nDownloading...\n" ;;
+#wget -P $HOME/.yq2 -O baseq2.zip http://www.dropbox.com/s/bh5co9nnmy0xf0n/baseq2.zip?dl=0 && unzip $HOME/baseq2.zip -d $HOME/.yq2
+wget -P $HOME -O baseq2.zip $Q2_PAK_URL
+wget -P $HOME -O q2_ogg.zip $Q2_OGG_URL
+unzip $HOME/baseq2.zip -d $DATA_DIR
+unzip $HOME/q2_ogg.zip -d $DATA_DIR/baseq2
+rm $HOME/baseq2.zip $HOME/q2_ogg.zip
+
+read -p "run with: $EXEC.Press [Enter] to continue..."
