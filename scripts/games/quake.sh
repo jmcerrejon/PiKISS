328e8d1fb514eb87c6208c22067d2e5d0ead0210
Jose Cerrejon <ulysess@gmail.com>
2015-03-27 07:35:32 +0100

    Fixing issues for ODROID-C1 & config.cfg

--- scripts/games/quake.sh
+++ scripts/games/quake.sh
@@ -2,7 +2,7 @@
 #
 # Description : Quake Pack (Install Quake 1/2/3/Server Ed, DarkPlace)
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.6.1 (25/Mar/15)
+# Version     : 0.7 (27/Mar/15)
 # Compatible  : Raspberry Pi 1 & 2 (tested), ODROID-C1 (tested)
 # Know bugs   : RPi with the latest Raspbian: black screen | ODROID can't change 640x480 resolution
 #
@@ -21,6 +21,7 @@ check_board || { echo "Missing file helper.sh. I've tried to download it for you
 Q1_DEMO_PAK_URL="http://www.quakeforge.net/files/quake-shareware-1.06.zip"
 QUAKE2_RPI_URL="http://pickle.gp2x.de/rpi/quake2_rpi.zip"
 Q2_PAK_URL="http://www.dropbox.com/s/bh5co9nnmy0xf0n/baseq2.zip?dl=0"
+Q2_CONFIG_URL="https://raw.githubusercontent.com/jmcerrejon/PiKISS/master/res/config.cfg"
 Q3_DEMO_PAK_URL="http://joshua14.homelinux.org/downloads/Q3-Demo-Paks.zip"
 DATA_DIR="$HOME/games/quake2"
 EXEC="$DATA_DIR/quake2"
@@ -28,15 +29,14 @@ EXEC="$DATA_DIR/quake2"
 quake2_ODROID(){
   DATA_DIR="$HOME/.yq2"
   EXEC="yquake2 in a Terminal or Menu > Games > Quake 2"
-  #sudo wget -P /etc/apt/sources.list.d http://oph.mdrjr.net/meveric/sources.lists/meveric-all-C1.list
-  #sudo wget -P /etc/apt/sources.list.d http://oph.mdrjr.net/meveric/sources.lists/meveric-all-main.list
   sudo wget -P /etc/apt/sources.list.d http://oph.mdrjr.net/meveric/sources.lists/meveric-all-testing.list
   sudo wget -O- http://oph.mdrjr.net/meveric/meveric.asc | sudo apt-key add -
   sudo apt-get update
   sudo apt-get install -y yquake2-odroid libglew-odroid
   [ ! -d $DATA_DIR/baseq2 ] && mkdir -p $DATA_DIR/baseq2
   cp /usr/local/share/yquake2/baseq2/game.so $DATA_DIR/baseq2
-  cd /usr/lib/arm-linux-gnueabihf/ && sudo ln -sf libEGL.so.1 libEGL.so
+  wget -P $DATA_DIR/baseq2 $Q2_CONFIG_URL
+  #cd /usr/lib/arm-linux-gnueabihf/ && sudo ln -sf libEGL.so.1 libEGL.so
 }
 
 quake2_Raspberry(){
@@ -74,9 +74,9 @@ case $retval in
   0)   echo -e "Installing...\n" ;;
   1)   clear ; read -p "Please copy into $DATA_DIR/baseq2 all the .pak files from the original game and run with: $EXEC. Press [Enter] to continue..." ; exit ;;
 esac
-
+#wget -P $HOME/.yq2 -O baseq2.zip http://www.dropbox.com/s/bh5co9nnmy0xf0n/baseq2.zip?dl=0 && unzip $HOME/baseq2.zip -d $HOME/.yq2
 wget -P $DATA_DIR -O baseq2.zip $Q2_PAK_URL
-unzip $DATA_DIR/baseq2.zip -d $DATA_DIR
-rm $DATA_DIR/baseq2.zip
+unzip $HOME/baseq2.zip -d $DATA_DIR
+rm $HOME/baseq2.zip
 
 read -p "Press [Enter] to continue..."
