e765cc819f58a9700d593050057b12d5eab558c0
Jose Cerrejon <ulysess@gmail.com>
2015-04-05 12:36:35 +0200

    Update distros path

--- scripts/info/check_lastmod_img.sh
+++ scripts/info/check_lastmod_img.sh
@@ -2,7 +2,7 @@
 #
 # Description : Check Last-Modified field from some distros images, so you can know if a distro is updated.
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1 (11/Mar/15)
+# Version     : 1.2 (11/Apr/15)
 #
 # OpenELEC: http://downloads.raspberrypi.org/openelec_latest
 # 
@@ -16,15 +16,14 @@ declare -a imgName=(
             'Raspbian'
             'PiDora'
 			'pipaOS'
-            'RaspBMC'
+            'OSMC RPi2'
 			'Arch Linux'
             'Risc OS'
 			'MinePeon'
 			'PiPlay (PiMAME)'
 			'ArkOS'
 			'Volumio'
-			'ScoutBot'
-			'OpenELEC'
+			'OpenELEC RPi2'
 			'Domoticz'
 )
 declare -a imgUrl=(
@@ -32,15 +31,14 @@ declare -a imgUrl=(
             'http://downloads.raspberrypi.org/raspbian_latest'
             'http://downloads.raspberrypi.org/pidora_latest'
 			'http://pipaos.mitako.eu/'
-            'http://download.raspbmc.com/downloads/bin/filesystem/prebuilt/raspbmc-final.img.gz'
+            'http://download.osmc.tv/installers/diskimages/OSMC_TGT_rbp2_20150315.img.gz'
 			'http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz'
             'http://downloads.raspberrypi.org/riscos_latest'
 			'http://sourceforge.net/projects/minepeon/files/latest/download?source=files'
 			'http://sourceforge.net/projects/pimame/files/latest/download?source=files'
 			'https://nyus.mirror.arkos.io/os/latest-rpi.tar.gz'
 			'http://sourceforge.net/projects/volumio/files/latest/download?source=files'
-			'http://sourceforge.net/projects/scoutbot/files/latest/download?source=files'
-			'http://releases.openelec.tv/OpenELEC-RPi2.arm-5.0.5.img.gz'
+			'http://releases.openelec.tv/OpenELEC-RPi2.arm-5.0.8.img.gz'
 			'http://sourceforge.net/projects/domoticz/files/latest/download?source=files'
 )
 
