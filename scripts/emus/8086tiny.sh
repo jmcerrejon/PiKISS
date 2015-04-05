256365ec261234057e9c1c03aaf6d218dc34b391
Jose Cerrejon <ulysess@gmail.com>
2015-04-05 11:23:50 +0200

    Fixed some issues

--- scripts/emus/8086tiny.sh
+++ scripts/emus/8086tiny.sh
@@ -2,12 +2,14 @@
 #
 # Description : Compile 8086tiny is a free, open source PC XT-compatible emulator/virtual machine written in C
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.9 (27/Jun/14)
+# Version     : 1.0 (5/Apr/15)
 #
 clear
 
-INSTALL_DIR="/home/$USER/games/"
-URL_FILE="http://rpix86.patrickaalto.com/rpix86.zip"
+. ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'http://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
+
+INSTALL_DIR="$HOME/games/"
 
 changeInstallDir(){
     echo "Enter new full path:"
@@ -17,7 +19,8 @@ changeInstallDir(){
 
 install(){
     sudo apt-get install -y libsdl1.2-dev
-    mkdir -p $INSTALL_DIR
+    SDL_fix_Rpi
+    mkdir -p $INSTALL_DIR && cd $_
     git clone https://github.com/adriancable/8086tiny.git
     cd 8086tiny/
     make
@@ -26,10 +29,10 @@ install(){
     exit
 }
 
-echo -e "Compile 8086tiny (latest)\n=========================\n· More Info: http://www.megalith.co.uk/8086tiny\n· Alley Cat game and FreeDOS included.\n\nInstall path: $INSTALL_DIR"
+echo -e "Compile 8086tiny (latest)\n=========================\n· More Info: http://www.megalith.co.uk/8086tiny\n· Alley Cat game and FreeDOS included.\n· 43.6 MB of additional disk space will be used.\n\nInstall path: $INSTALL_DIR"
 while true; do
     echo " "
-    read -p "Is it right? [y/n] " yn
+    read -p "Is it right? [(Y)es/(N)o/(E)xit] " yn
     case $yn in
     [Yy]* ) echo "Installing, please wait..." && install;;
     [Nn]* ) changeInstallDir;;
