cb2d79b7d9686a3eeb71c5ddfc2e758ebff83d60
Jose Cerrejon <ulysess@gmail.com>
2015-04-05 12:27:54 +0200

    Some minor fixes

--- scripts/emus/pce.sh
+++ scripts/emus/pce.sh
@@ -2,49 +2,42 @@
 #
 # Description : PCE-CD emulation by Vanfanel
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0 (26/Jun/14)
+# Version     : 1.1 (5/Apr/15)
+# Compatible  : Raspberry Pi 1 & 2 (tested)
 #
 # HELP        · http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=80357
 #
 clear
 
-INSTALL_DIR="/home/$USER/games"
+INSTALL_DIR="$HOME/games"
 URL_FILE="https://www.dropbox.com/s/vzn3932ugh0q4rx/pce_rpi.zip"
+ROM_URL='http://aetherbyte.com/files/Aetherbyte_Reflectron.zip'
 
-validate_url(){
-    if [[ `wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+playgame()
+{
+    if [[ -f $HOME/games/pce/Aetherbyte_Reflectron.zip ]]; then
+        read -p "Do you want to play Reflectron now? [y/n] " option
+        case "$option" in
+            y*) cd $HOME/games/pce && ./pce Aetherbyte_Reflectron.zip ;;
+        esac
+    fi
 }
 
-changeInstallDir(){
-    echo "Enter new full path:"
-    read INSTALL_DIR
-    echo "New path: $INSTALL_DIR"
-}
-
-install(){
-    if [[ $(validate_url $URL_FILE) != "true" ]] ; then
-        echo "Sorry, the emulator is not available here: $URL_FILE. Visit the website to download it manually."
-        exit
-    else
-        #sudo apt-get install -y libsdl1.2debian libsdl-mixer1.2 libsdl-ttf2.0-0
+install()
+{
+    #sudo apt-get install -y libsdl1.2debian libsdl-mixer1.2 libsdl-ttf2.0-0
+    if [[ ! -e $HOME/games/pce ]]; then
         mkdir -p $INSTALL_DIR && cd $_
         wget -qO- -O tmp.zip $URL_FILE && unzip tmp.zip && rm tmp.zip
         cd pce/
-        wget 'http://aetherbyte.com/files/Aetherbyte_Reflectron.zip'
-        echo "Done!. To play, go to install path and type: ./pce Aetherbyte_Reflectron.zip"
+        wget $ROM_URL
     fi
+    echo "Done!. To play, go to install path and type: ./pce Aetherbyte_Reflectron.zip"
+    playgame
+    clear
     read -p "Press [Enter] to continue..."
     exit
 }
 
-echo -e "PCE-CD emulation by Vanfanel\n============================\n\n· More Info: http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=80357\n· + Game Reflectron by Aetherbyte\n· Works with compressed roms (.zip)\n\nInstall path: $INSTALL_DIR/pce"
-while true; do
-    echo " "
-    read -p "Is it right? [y/n] " yn
-    case $yn in
-    [Yy]* ) echo "Installing, please wait..." && install;;
-    [Nn]* ) changeInstallDir;;
-    [Ee]* ) exit;;
-    * ) echo "Please answer (y)es, (n)o or (e)xit.";;
-    esac
-done
+echo -e "PCE-CD emulation by Vanfanel\n============================\n\n· More Info: http://www.raspberrypi.org/forums/viewtopic.php?f=78&t=80357\n· + Game Reflectron by Aetherbyte\n· Works with compressed roms (.zip)\n· Install path: $INSTALL_DIR/pce\n\nInstalling, please wait..."
+install
