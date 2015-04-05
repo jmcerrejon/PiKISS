572a453aa1f515c720776e06e8ad5c446c1edd2b
Jose Cerrejon <ulysess@gmail.com>
2015-04-05 21:07:59 +0200

    Added Fix SDL

--- scripts/others/fixes.sh
+++ scripts/others/fixes.sh
@@ -2,9 +2,13 @@
 #
 # Description : Fix some problems with the Raspbian OS
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.3 (9/Sep/14)
+# Version     : 0.4 (5/Apr/15)
+# Compatible  : Raspberry Pi 1 (Doesn't work) & 2 (tested. OK.)
 #
 clear
+
+. ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'http://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
 wHEIGHT=10
@@ -27,11 +31,13 @@ do
 		--menu 		"Fix some problems with the Raspbian OS. Choose your fix or Exit:" $wHEIGHT 90 $wHEIGHT \
 		Sound		"ALSA lib pcm.c:2217:(snd_pcm_open_noupdate) Unknown PCM cards.pcm.front" \
 		Black		"Black out the local terminal (ideal for omxplayer)" \
+		SDL		"Fix SDL1.2 black screen" \
 		Exit 		"Exit to the shell" 2>"${tempfile}"
 
 	case $(<"${tempfile}") in
 		Sound)	SOUND ;;
 		Black)	BLACK_SCREEN ;;
+		SDL)	SDL_fix_Rpi ;;
 		Exit)	exit ;;
 	esac
 done
