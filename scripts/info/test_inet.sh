db3cc349824626b2395b3ba707cb5b3b1446844d
Jose Cerrejon <ulysess@gmail.com>
2015-04-09 16:49:40 +0200

    check speed to 1.0

--- scripts/info/test_inet.sh
+++ scripts/info/test_inet.sh
@@ -2,17 +2,11 @@
 #
 # Description : Test Internet Speed thanks to Matt Martz
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.9 (5/Apr/15)
+# Version     : 1.0 (9/Apr/15)
 # Tested      : Raspbian/Debian/Ubuntu, OSX
 #
-# IMPROVEMENT : dialog menu with 2 methods.
 #
 clear
-
-
-method2(){
-	wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip
-}
 
 PYTHON_SCRIPT="https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py"
 
@@ -21,7 +15,7 @@ clear
 
 echo -e "Test Internet bandwidth thanks to Matt Martz\n============================================\n· More Info at: https://github.com/sivel/speedtest-cli\n"
 
-python speedtest_cli.py
+python $HOME/speedtest_cli.py
 rm $HOME/speedtest_cli.py
 
 read -p "Press [Enter] to continue..."
