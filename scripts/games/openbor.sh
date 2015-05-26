f48f16b969495419d869ba25e2574dfb37cf5508
Jose Cerrejon <ulysess@gmail.com>
2015-05-26 06:58:03 +0200

    Minor string fix

--- scripts/games/openbor.sh
+++ scripts/games/openbor.sh
@@ -6,8 +6,11 @@
 # Compatible  : Raspberry Pi 1 & 2 (tested), Debian
 # Help				: https://www.raspberrypi.org/forums/viewtopic.php?f=78&t=26859&start=25
 # 					  · Games: https://mega.co.nz/#F!4xMgTDTA!bnfrA4RapYRvS31jSak3IQ
-#
+# http://cavernofcreativity.com/Atlas/xthreads_attach.php/432_1432021152_aa0362dd/9920f797b8fdb0dbdce5b886be3d582d/ARAH%202015.zip
 clear
+
+. ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'http://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 mkDesktopEntry(){
 	if [[ ! -e /usr/share/applications/openbor.desktop ]]; then
@@ -17,9 +20,12 @@ mkDesktopEntry(){
 
 mkDesktopEntry
 
+SDL_fix_Rpi
+sudo apt-get install -y libsdl-gfx1.2-4
+
 mkdir -p $HOME/games
 wget http://misapuntesde.com/res/openbor_by_ulysess.tar.gz
-tar xvf openbor*
+tar xzvf openbor*
 
-echo -e "\nDone!. First copy pak files inside Paks directory and run ./unpack.sh\n To play, run: /games/openbor/openbor_rpi"
+echo -e "\nDone!.\n· First copy pak files inside Paks directory and run ./unpack.sh\n· To play, run: /games/openbor/openbor_rpi"
 read -p "Press [ENTER] to continue..."
