4b82d4a7634e8c0106552645202f0406cc7eb9a4
Jose Cerrejon <ulysess@gmail.com>
2015-05-29 09:44:48 +0200

    Minor changes, no matter

--- res/cRasp.sh
+++ res/cRasp.sh
@@ -2,7 +2,7 @@
 #
 # Description : Personal script to make my custom Raspbian
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.3 (23/May/15)
+# Version     : 1.3 (26/May/15)
 # Compatible  : Raspberry Pi 1 & 2 (tested)
 #
 # Best method to get, SSH into RPi and: wget -O pre.sh https://goo.gl/qQ3JDv && chmod +x pre.sh && ./pre.sh
@@ -29,6 +29,17 @@ sudo sed -i 's/.*tty1.*/&\n1:2345:respawn:\/bin\/login -f pi tty1 <\/dev\/tty1> 
 wget -P /tmp http://malus.exotica.org.uk/~buzz/pi/sdl/sdl1/deb/rpi1/libsdl1.2debian_1.2.15-8rpi_armhf.deb
 sudo dpkg -i /tmp/libsdl1.2debian_1.2.15-8rpi_armhf.deb
 sudo rm /tmp/libsdl1.2debian_1.2.15-8rpi_armhf.deb
+
+# Disable partition "swap"
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+
+# Avoid SD corruption
+sudo sed -i 's/atime/noatime,nodiratime/g' /etc/fstab
+
+# Automatic fsck on start
+#sudo sed -i 's/#FSCKFIX=no/FSCKFIX=yes/g' /etc/default/rcS && grep "FSCKFIX=yes" /etc/default/rcS | wc -l
 
 # Other stuff
 sudo apt-file update
