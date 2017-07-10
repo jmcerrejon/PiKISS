acb2914503ad9751725727f332214c0e00edc43a
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2017-07-10 13:14:04 +0200

    Added to Wordpress SQLite.

--- scripts/server/fwork.sh
+++ scripts/server/fwork.sh
@@ -2,11 +2,11 @@
 #
 # Description : Install a Framework,CMS to the web server
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.6 (10/Jul/17)
+# Version     : 0.7 (10/Jul/17)
 #
 # Help        · Wordpress: https://github.com/raspberrypi/documentation/blob/master/usage/wordpress.md
-#             ·     Ghost: http://geekytheory.com/ghost-blog-en-raspberry-pi/
-#             ·   PyPlate: http://pplware.sapo.pt/linux/dica-como-ter-o-seu-proprio-site-no-raspberry-pi/
+#             · PyPlate: http://pplware.sapo.pt/linux/dica-como-ter-o-seu-proprio-site-no-raspberry-pi/
+#             · https://darryldias.me/wordpress-with-sqlite/
 #
 clear
 
@@ -14,48 +14,65 @@ URL_GHOST="https://github.com/TryGhost/Ghost/releases/download/0.11.10/Ghost-0.1
 URL_WORDPRESS="https://wordpress.org/latest.tar.gz"
 URL_NODEJS="https://nodejs.org/dist/latest/node-v8.1.3-linux-armv7l.tar.gz"
 
+sqlite_integration(){
+  WP_INSTALL="/var/www/html/wordpress"
+  USER_DB="$USER/sc/db_wordpress"
+  wget -P $WP_INSTALL/wp-content/plugins/ https://downloads.wordpress.org/plugin/sqlite-integration.1.8.1.zip
+  unzip $WP_INSTALL/wp-content/plugins/sqlite*.zip
+  cp $WP_INSTALL/wp-content/plugins/sqlite-integration/db.php ../../
+  cp $WP_INSTALL/wp-config-sample.php ./wp-config.php
+  sudo mkdir -p $USER/sc/db_wordpress
+  echo "define('DB_FILE', 'wordpress');" >> $WP_INSTALL/wp-config.php
+  echo "define('DB_DIR', '${USER_DB}'); " >> $WP_INSTALL/wp-config.php
+  echo -e "SQLite integration installed.\n"
+}
+
 wordpress(){
-    cd /var/www
-    sudo chown $USER: .
-    wget $URL_WORDPRESS
-    tar xzf ${URL_WORDPRESS##*/}
-    rm ${URL_WORDPRESS##*/}
-    echo "Installed on /var/www/wordpress directory"
+  cd /var/www/html || return
+  sudo chown $USER: .
+  wget $URL_WORDPRESS
+  tar xzf ${URL_WORDPRESS##*/}
+  rm ${URL_WORDPRESS##*/}
+  read -p "Enable SQLite integration? [y/n] " option
+  case "$option" in
+    y*) sqlite ;;
+  esac
+  echo "Installed on /var/www/wordpress directory"
+  read -p "Wordpress installed. Press [ENTER] to exit..."
 }
 
 nodejs(){
-    cd /usr/local
-    sudo wget $URL_NODEJS
-    sudo tar xvzf /usr/local/${URL_NODEJS##*/} --strip=1
-    sudo rm /usr/local/${URL_NODEJS##*/}
-    # Update if old version is installed
-    sudo npm cache clean -f
-    sudo npm install -g n
-    sudo n stable
-    echo "Press [Control+D] to return as normal user..."
-    node --version
+  cd /usr/local || exit
+  sudo wget $URL_NODEJS
+  sudo tar xvzf /usr/local/${URL_NODEJS##*/} --strip=1
+  sudo rm /usr/local/${URL_NODEJS##*/}
+  # Update if old version is installed
+  sudo npm cache clean -f
+  sudo npm install -g n
+  sudo n stable
+  echo "Press [Control+D] to return as normal user..."
+  node --version
 }
 
 ghost(){
-    sudo mkdir -p /var/www/ghost
-    cd /var/www/ghost
-    sudo chown $USER: .
-    wget -qO- -O tmp.zip $URL_GHOST && unzip -o tmp.zip && rm tmp.zip
-    sudo npm install --production
-    sudo npm start
+  sudo mkdir -p /var/www/html/ghost && cd $_ || return
+  sudo chown $USER: .
+  wget -qO- -O tmp.zip $URL_GHOST && unzip -o tmp.zip && rm tmp.zip
+  sudo npm install --production
+  sudo npm start
 }
 
-read -p "Wordpress (latest)? [y/n]" option
+read -p "Wordpress (latest)? [y/n] " option
 case "$option" in
-    y*) wordpress ;;
+  y*) wordpress ;;
 esac
 
-read -p "Node.js (latest)? [y/n]" option
+read -p "Node.js (latest)? [y/n] " option
 case "$option" in
-    y*) nodejs ;;
+  y*) nodejs ;;
 esac
 
-read -p "GHOST (${URL_GHOST##*/})? [y/n]" option
+read -p "GHOST (${URL_GHOST##*/})? [y/n] " option
 case "$option" in
-    y*) ghost ;;
+  y*) ghost ;;
 esac
