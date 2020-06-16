ab3d06dc96937419fe99dda03c65b155cd3cefab
Jose Cerrejon <ulysess@gmail.com>
2020-06-16 13:31:01 +0200

    👌 IMPROVE: Change PiSNES (pretty old and outdated) for Snes9x 1.60

--- scripts/emus/pisnes.sh
+++ scripts/emus/pisnes.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
 #
-# Description : PiSNES for Raspberry Pi by Squid
+# Description : Snes9X for Raspberry Pi by Squid
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.9.3 (5/Apr/15)
-# Compatible  : Raspberry Pi 1 (Doesn't work) & 2 (tested. OK.)
+# Version     : 1.0.0 (16/Jun/20)
+# Compatible  : Raspberry Pi 2-4
 #
-clear
-
 . ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
+clear
 
-INSTALL_DIR="$HOME/games/pisnes/"
-URL_FILE="https://sourceforge.net/projects/pisnes/files/latest/download?source=files"
+INSTALL_DIR="$HOME/games/snes9x"
+URL_FILE="https://misapuntesde.com/res/snes9x_1-60.tar.gz"
 GAME_URL="https://www.dropbox.com/s/b6tl84p3a17w5ab/UwolQuestForMoney.sfc?dl=0"
 
-playgame()
-{
+playgame(){
     read -p "Do you want to play Uwol Quest For Money now? [y/n] " option
     case "$option" in
-        y*) $INSTALL_DIR/snes9x $INSTALL_DIR/roms/uwol.sfc ;;
+        y*) $INSTALL_DIR/snes9x $INSTALL_DIR/roms/uwol.sfc -fullscreen -maxaspect;;
     esac
 }
 
-changeInstallDir()
-{
+changeInstallDir(){
     echo "Enter new full path:"
     read INSTALL_DIR
     echo "New path: $INSTALL_DIR"
 }
 
-install()
-{
-    mkdir -p $INSTALL_DIR && cd $_
-    wget -qO- -O tmp.zip $URL_FILE && unzip -o tmp.zip && rm tmp.zip
-    chmod 777 ./snes9x ./snes9x.cfg ./roms ./skins
+install(){
+    mkdir -p $HOME/games && cd $_
+    wget -qO- -O tmp.tar.gz $URL_FILE && tar -xzvf tmp.tar.gz && rm tmp.tar.gz
     echo -e "\nInstalling game Uwol Quest For Money (uwol.sfc) on $INSTALL_DIR\n\n" && wget -q -O $INSTALL_DIR/roms/uwol.sfc  $GAME_URL
-    echo -e "Done!. To play go to install path, copy any rom to /roms directory and type: ./snes9x <rom name>\nFor example, ./snes9x roms/uwol.sfc"
+    echo -e "Done!. To play go to install path, copy any rom to /roms directory and type: ./snes9x <rom name>\nFor example, ./snes9x roms/uwol.sfc -fullscreen -maxaspect"
     playgame
     read -p "Press [Enter] to continue..."
     exit
 }
 
-echo -e "Install PiSNES (Version 1.39)\n=============================\nMore Info: https://sourceforge.net/projects/pisnes\n\nInstall path: $INSTALL_DIR"
+echo -e "Install Snes9X (Version 1.60)\n=============================\n· More Info: https://github.com/snes9xgit/snes9x\n\n· Install free game Uwol Quest For Money thks to Mojon Twins.\n\n· Install path: $INSTALL_DIR"
 while true; do
     echo " "
     read -p "Is it right? [y/n] " yn
