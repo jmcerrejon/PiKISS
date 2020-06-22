03bfedf5983ace218ebda5da5d3f2cdee08ff805
Jose Cerrejon <ulysess@gmail.com>
2020-06-22 14:43:29 +0200

    👌 IMPROVE: Check if PiKISS is already installed

--- res/install.sh
+++ res/install.sh
@@ -3,9 +3,14 @@
 # Description : Easy install PiKISS
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
 # Version     : 1.0 (22/Jun/20)
-# TODO		  : Check is a RPi, Check Git is installed, Check the OS is Debian based.
+# TODO		  : Check is a RPi, Check the OS is Debian based.
 clear
 
-echo -e "\nPiKISS======\nInstalling at $HOME/piKISS. Please wait..."
+if [ -d "$HOME/piKISS" ]; then
+	cd "$HOME/piKISS" && ./piKiss.sh
+fi
+
+echo -e "\nPiKISS\n======\nInstalling at $HOME/piKISS. Please wait...\n"
+sudo apt install -y dialog
 git clone https://github.com/jmcerrejon/PiKISS.git piKiss && cd "$_"
 ./piKiss.sh
