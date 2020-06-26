abcad507c48b9ba572302bf2a6eb85b4d5be1392
Jose Cerrejon <ulysess@gmail.com>
2020-06-26 18:17:33 +0200

    👌 IMPROVE: Refactor

--- res/cRasp.sh
+++ res/cRasp.sh
@@ -2,48 +2,49 @@
 #
 # Description : Personal script to make my custom Raspbian
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.3 (26/May/15)
-# Compatible  : Raspberry Pi 1 & 2 (tested)
+# Version     : 1.3.1 (26/Jun/20)
+# Compatible  : Raspberry Pi 1-4 (tested)
 #
 # Best method to get, SSH into RPi and: wget -O pre.sh https://goo.gl/qQ3JDv && chmod +x pre.sh && ./pre.sh
 #
 clear
 
-sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install -y mc htop apt-file unrar-free
-sudo apt-get remove -y wolfram-engine
+sudo apt-get update
 
+read -p "Do you want to dist upgrade now? [y/n] " option
+case "$option" in
+	y*) sudo apt-get dist-upgrade -y ;;
+esac
 
+echo -e "\nInstalling some packages...\n"
+sudo apt-get install -y mc htop apt-file sshfs dialog
+sudo apt-get -y autoremove
+
+echo -e "\nAdding useful alias...\n"
 # Some useful alias
-sed -i "/# Alias definitions/i\alias upd='sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get clean && sudo apt-get autoclean'" $HOME/.bashrc
-sed -i "/# Alias definitions/i\alias reboot='sudo reboot'" $HOME/.bashrc
-sed -i "/# Alias definitions/i\alias ins='sudo apt-get install -y '" $HOME/.bashrc
-sed -i "/# Alias definitions/i\alias halt='sudo halt'" $HOME/.bashrc
-sed -i "/# Alias definitions/i\alias rm='sudo rm -rf '" $HOME/.bashrc
+cp ./.bash_aliases $HOME/.bash_aliases
 
-# autologin
-sudo sed -i '/1:2345/s/^/#/' /etc/inittab
-sudo sed -i 's/.*tty1.*/&\n1:2345:respawn:\/bin\/login -f pi tty1 <\/dev\/tty1> \/dev\/tty1 2>\&1/' /etc/inittab
+disableSwap() {
+	# Disable partition "swap"
+	sudo dphys-swapfile swapoff
+	sudo dphys-swapfile uninstall
+	sudo update-rc.d dphys-swapfile remove
+}
 
-# Fix SDL
-wget -P /tmp https://malus.exotica.org.uk/~buzz/pi/sdl/sdl1/deb/rpi1/libsdl1.2debian_1.2.15-8rpi_armhf.deb
-sudo dpkg -i /tmp/libsdl1.2debian_1.2.15-8rpi_armhf.deb
-sudo rm /tmp/libsdl1.2debian_1.2.15-8rpi_armhf.deb
-
-# Disable partition "swap"
-sudo dphys-swapfile swapoff
-sudo dphys-swapfile uninstall
-sudo update-rc.d dphys-swapfile remove
-
-# Avoid SD corruption
-sudo sed -i 's/atime/noatime,nodiratime/g' /etc/fstab
+read -p "Do you want to disable SWAP? [y/n] " option
+case "$option" in
+	y*) disableSwap ;;
+esac
 
 # Automatic fsck on start
 #sudo sed -i 's/#FSCKFIX=no/FSCKFIX=yes/g' /etc/default/rcS && grep "FSCKFIX=yes" /etc/default/rcS | wc -l
 
 # Other stuff
 sudo apt-file update
-# Sometime rpi-update broke my Raspbian, so be carefull
-#sudo rpi-update
 
+# Sometime rpi-update broke my Raspbian, so be carefull
+sudo rpi-update
+
+echo -e "\nThe system is going to reboot in 5 seconds, pray...\n"
+sleep 5
 sudo reboot
--- scripts/games/dune2.sh
+++ scripts/games/dune2.sh
@@ -1,43 +1,62 @@
 #!/bin/bash
 #
-# Description : Download & Install Dune 2
+# Description : Dune 2
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.3 (16/Sep/19)
+# Version     : 1.3.1 (26/Jun/20)
 # Compatible  : Raspberry Pi 1-4 (tested)
 #
+. ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 clear
+
 DATA_DIR="$HOME/.config/dunelegacy/data"
-DUNE2_GAME="https://www.bestoldgames.net/download/bgames/dune-2.zip"
+DUNE2_GAME="http://www.bestoldgames.net/download/bgames/dune-2.zip"
 
-# Dune 2 Legacy
-echo "Downloading Dune Legacy..."
-wget -O /tmp/dunelegacy_0.96.4_armhf.deb https://sourceforge.net/projects/dunelegacy/files/dunelegacy/0.96.4/dunelegacy_0.96.4_armhf.deb/download
-sudo dpkg -i /tmp/dunelegacy_0.96.4_armhf.deb
+downloadData() {
+	echo -e "\nDownloading Dune Data,...\n"
+	# Dune 2 Abandonware borrowed from bestoldgames.net
+	wget -O /tmp/dune2.zip $DUNE2_GAME
 
-echo "Installing dependencies..."
-sudo apt install -y libopusfile0 libsdl2-mixer-2.0-0
+	mkdir -p $DATA_DIR
+	unzip /tmp/dune2.zip -d $DATA_DIR *.PAK
 
-dialog --backtitle "piKiss" \
-         --title     "[ Download Dune 2 Abandonware ]" \
-         --yes-label "Yes" \
-         --no-label  "No" \
-         --yesno     "You need the original .PAK files from the original game. Do you want to download? (In some countries the laws may consider it pirate software)" 7 55
+	# Cleaning the House
+	rm /tmp/dune2.*
+}
 
-  retval=$?
+installGame() {
+	echo -e "\nInstalling Dune Legacy, please wait...\n"
+	wget -O /tmp/dunelegacy_0.96.4_armhf.deb https://sourceforge.net/projects/dunelegacy/files/dunelegacy/0.96.4/dunelegacy_0.96.4_armhf.deb/download
+	sudo dpkg -i /tmp/dunelegacy_0.96.4_armhf.deb
+}
 
-  case $retval in
-    0)   echo -e "Installing...\n" ;;
-    1)   clear ; read -p "Please copy into ~/.config/dunelegacy/data all the .PAK files from the original game and run with: dunelegacy. Press [Enter] to continue..." ; exit ;;
-  esac
+init() {
+	echo -e "\nInstalling dependencies...\n"
+	if ! isPackageInstalled libopusfile0; then
+		sudo apt install -y libopusfile0
+	fi
+	if ! isPackageInstalled libsdl2-mixer-2.0-0; then
+		sudo apt install -y libsdl2-mixer-2.0-0
+	fi
+}
 
-# Dune 2 Abandonware borrowed from bestoldgames.net
-wget -O /tmp/dune2.zip $DUNE2_GAME
+askDownloadDataFiles() {
+	dialog --backtitle "piKiss" \
+		--title     "[ Download Dune 2 Abandonware ]" \
+		--yes-label "Yes" \
+		--no-label  "No" \
+		--yesno     "You need the original .PAK files from the original game. Do you want to download? (In some countries the laws may consider it pirate software)" 7 55
+	retval=$?
 
-mkdir -p $DATA_DIR
-unzip /tmp/dune2.zip -d $DATA_DIR *.PAK
+	case $retval in
+		0) echo -e "Installing...\n"; downloadData; return 0 ;;
+		1) clear ; echo --e "\nPlease copy into ~/.config/dunelegacy/data all the .PAK files from the original game. " ; return 0 ;;
+	esac
+}
 
-# Cleaning the House
-rm /tmp/dune2.*
+init
+installGame
+askDownloadDataFiles
 
-echo -e "\nType dunelegacy to play the game. Enjoy!"
+echo -e "\nType dunelegacy to play the game Or Go to Menu > Games > Dunelegacy.\n"
 read -p "Press [Enter] to continue..."
