82298594be0e9c08938145199c42946a0a6d74d2
Jose Cerrejon <ulysess@gmail.com>
2020-07-18 10:02:38 +0200

    👌 IMPROVE: Add extract_url_from_file

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -576,6 +576,16 @@ message_magic_air_copy() {
 }
 
 #
+# Extract row from a file
+#
+extract_url_from_file() {
+	local tmp_file=/tmp/shareware
+	wget -qO "$tmp_file" bit.ly/2WzjwaC
+	sed "$1q;d" "$tmp_file"
+	rm "$tmp_file"
+}
+
+#
 # Extract all kind of compressed files
 #
 extract() {
@@ -589,9 +599,9 @@ extract() {
 		*.rar) unrar x "$1" ;;
 		*.gz) gunzip "$1" ;;
 		*.tar) tar xvf "$1" ;;
-		*.zip) unzip "$1" ;;
+		*.zip) unzip -qq "$1" ;;
 		*.Z) uncompress "$1" ;;
-		*.7z) 7z x "$1" ;;
+		*.7z) p7zip -d "$1" ;;
 		*.exe) cabextract "$1" ;;
 		*) echo "'$1': unrecognized file compression" ;;
 		esac
