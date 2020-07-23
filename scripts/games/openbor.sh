ef834caaf876b52bd114d7ecea2c3c35c09be727
Jose Cerrejon <ulysess@gmail.com>
2020-07-23 11:11:49 +0200

    🐛 FIX: Add missing library

--- scripts/games/openbor.sh
+++ scripts/games/openbor.sh
@@ -2,19 +2,17 @@
 #
 # Description : OpenBOR
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.2 (06/Sep/16)
-# Compatible  : Raspberry Pi 1, 2 & 3 (tested), Debian
-# Help				: https://www.raspberrypi.org/forums/viewtopic.php?f=78&t=26859&start=25
-# 					  · Games: https://mega.co.nz/#F!4xMgTDTA!bnfrA4RapYRvS31jSak3IQ
-# https://cavernofcreativity.com/Atlas/xthreads_attach.php/432_1432021152_aa0362dd/9920f797b8fdb0dbdce5b886be3d582d/ARAH%202015.zip
-clear
-
+# Version     : 1.2.1 (23/Jul/20)
+# Compatible  : Raspberry Pi 1-4 (tested)
+# Help		  : https://www.raspberrypi.org/forums/viewtopic.php?f=78&t=26859&start=25
+#
 . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-mkDesktopEntry(){
+mkDesktopEntry() {
 	if [[ ! -e ~/.local/share/applications/openbor.desktop ]]; then
-cat << EOF > ~/.local/share/applications/openbor.desktop
+		cat <<EOF >~/.local/share/applications/openbor.desktop
 [Desktop Entry]
 Name=OpenBOR
 Exec=/home/pi/games/openbor/openbor_rpi
@@ -25,19 +23,18 @@ Categories=Game;ActionGame;
 EOF
 	fi
 }
-mkDesktopEntry
 
-SDL_fix_Rpi
-sudo apt-get install -y libsdl-gfx1.2-5
+sudo apt install -y libsdl-gfx1.2-5 libpng12-0
+mkDesktopEntry
 
 if [[ ! -e /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.13 ]]; then
 	sudo ln -s /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.15 /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.13
 fi
 
-mkdir -p $HOME/games && cd $_
-wget https://misapuntesde.com/res/openbor_by_ulysess.tar.gz
+mkdir -p "$HOME"/games && cd "$_"
+wget -q https://misapuntesde.com/res/openbor_by_ulysess.tar.gz
 tar xzvf openbor*
 rm openbor_by_ulysess.tar.gz
 
 echo -e "\nDone!.\n· First copy pak files inside Paks directory and run ./unpack.sh\n· To play, run: $HOME/games/openbor/openbor_rpi"
-read -p "Press [ENTER] to continue..."
+exit_message
--- scripts/games/xump.sh
+++ scripts/games/xump.sh
@@ -2,26 +2,28 @@
 #
 # Description : Xump
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0 (07/Sep/16)
-# Compatible  : Raspberry Pi 1, 2 & 3 (tested)
+# Version     : 1.0.1 (23/Jul/20)
+# Compatible  : Raspberry Pi 1-4 (tested)
 #
+. ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 INSTALL_DIR="/home/$USER/games/xump/"
 URL_FILE="https://www.retroguru.com/xump/xump-v.latest-raspberrypi.zip"
 
-if  which $INSTALL_DIR/xump_rpi >/dev/null ; then
-    read -p "Warning!: Xump already installed. Press [ENTER] to exit..."
-    exit
+if which "$INSTALL_DIR"/xump_rpi >/dev/null; then
+	read -p "Warning!: Xump already installed. Press [ENTER] to exit..."
+	exit
 fi
 
-validate_url(){
-    if [[ `wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+validate_url() {
+	if [[ $(wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then echo "true"; fi
 }
 
-generateIcon(){
-    if [[ ! -e ~/.local/share/applications/Xump.desktop ]]; then
-cat << EOF > ~/.local/share/applications/Xump.desktop
+generateIcon() {
+	if [[ ! -e ~/.local/share/applications/Xump.desktop ]]; then
+		cat <<EOF >~/.local/share/applications/Xump.desktop
 [Desktop Entry]
 Name=Xump
 Exec=/home/pi/games/xump/xump_rpi
@@ -31,35 +33,37 @@ Comment=Xump - The Final Run is a simple multi-platform puzzler by Retroguru
 Categories=Game;ActionGame;
 Path=/home/pi/games/xump/
 EOF
-    fi
+	fi
 }
 
-install(){
-    if [[ $(validate_url $URL_FILE) != "true" ]] ; then
-        read -p "Sorry, the game is not available here: $URL_FILE. Visit the website to download it manually."
-        exit
-    else
-        mkdir -p $INSTALL_DIR && cd $_
-        wget -O /tmp/xump.zip $URL_FILE && unzip -o /tmp/xump.zip -d $INSTALL_DIR && rm /tmp/xump.zip
-        chmod +x xump_rpi
-        echo "Generating icon..."
-        generateIcon
-        echo -e "Done!. To play, on Desktop go to Menu > Games or via terminal, go to $INSTALL_DIR and type: ./xump_rpi\n\nEnjoy!"
-    fi
-    read -p "Press [Enter] to continue..."
-    exit
+install() {
+	if [[ $(validate_url $URL_FILE) != "true" ]]; then
+		read -p "Sorry, the game is not available here: $URL_FILE. Visit the website to download it manually."
+		exit_message
+	fi
+
+	sudo apt install -y libsdl-mixer1.2
+
+	mkdir -p "$INSTALL_DIR" && cd "$_"
+	wget -O /tmp/xump.zip $URL_FILE && unzip -o /tmp/xump.zip -d "$INSTALL_DIR" && rm /tmp/xump.zip
+	chmod +x xump_rpi
+	echo "Generating icon..."
+	generateIcon
+	echo -e "Done!. To play, on Desktop go to Menu > Games or via terminal, go to $INSTALL_DIR and type: ./xump_rpi\n\nEnjoy!"
+	
+	exit_message
 }
 
-echo "Install Xump(Raspberry Pi version)"
-echo "=================================="
+echo "Install Xump (Raspberry Pi version)"
+echo "==================================="
 echo -e "More Info: https://www.retroguru.com/xump/\n\nInstall path: $INSTALL_DIR"
 while true; do
-    echo " "
-    read -p "Proceed? [y/n] " yn
-    case $yn in
-    [Yy]* ) echo "Installing, please wait..." && install;;
-    [Nn]* ) exit;;
-    [Ee]* ) exit;;
-    * ) echo "Please answer (y)es, (n)o or (e)xit.";;
-    esac
+	echo " "
+	read -p "Proceed? [y/n] " yn
+	case $yn in
+		[Yy]*) echo "Installing, please wait..." && install ;;
+		[Nn]*) exit ;;
+		[Ee]*) exit ;;
+		*) echo "Please answer (y)es, (n)o or (e)xit." ;;
+	esac
 done
