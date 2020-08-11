5467caa5cbce762de0e0959431542af1cf7dfcd4
Jose Cerrejon <ulysess@gmail.com>
2020-08-11 11:10:06 +0200

    👌 IMPROVE: Nagios 4. Refactor code

--- scripts/server/nagios.sh
+++ scripts/server/nagios.sh
@@ -1,64 +1,55 @@
 #!/bin/bash
 #
-# Description : Install Nagios3
+# Description : Install Nagios4
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.1 (12/Sep/16)
-# Compatible  : Raspberry Pi 1, 2 & 3 (tested)
+# Version     : 1.0.0 (11/Aug/20)
+# Compatible  : Raspberry Pi 1-4 (tested)
 #
-#    https://netcodger.wordpress.com/2015/03/26/nagios-3-on-a-raspberry-pi-2/
+# TODO        : compile from source. Check https://pimylifeup.com/raspberry-pi-nagios/
 #
+. ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+clear
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-# Make sure this script only runs on a Raspberry Pi.
-if ! uname -a | grep "raspberrypi"; then
-     echo "This script only run on a Raspberry Pi."
-     read -p 'Press [ENTER] to continue...'
-     exit -1
-fi
+PACKAGES=(nagios4)
+PACKAGES_DEV=(autoconf build-essential wget unzip apache2 apache2-utils php libgd-dev snmp libnet-snmp-perl gettext libssl-dev wget bc gawk dc libmcrypt-dev)
+IP=$(get_ip)
+URL="http://${IP}/nagios"
 
-# Are you sure?
-echo "This script is for installing Nagios 3 on a new Raspberry Pi installatoin."
-echo "Installing on a modified system could overwrite any previous modifications."
-read -p "Are you sure you want to continue? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 1
-fi
+post_install() {
+    sudo usermod -a -G nagios www-data
+    sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+    echo
+    read -p "Do you want to reboot? (y/N) " response
+    if [[ $response =~ [Yy] ]]; then
+        sudo reboot
+    fi
+    exit_message
+}
 
-sudo apt-get -qq update
+install() {
+    echo
+    echo -e "\nInstalling Nagios, please wait..."
+    sudo apt-get -qq update
+    sudo apt -y full-upgrade
+    sudo dpkg-reconfigure tzdata
+    installPackagesIfMissing "${PACKAGES[@]}"
+}
 
-# Prompt user to set timezone as necessary.
-sudo dpkg-reconfigure tzdata
-
-# Install Nagios 3.
-sudo apt install -y nagios3
-
-
-# Optional install Nuvola skin.
-wget "https://exchange.icinga.org/exchange/Nuvola+Style/files/145/nagios-nuvola-1.0.3.tar.gz"
-mkdir nuvola
-cd nuvola
-tar -xzvf ../nagios-nuvola-1.0.3.tar.gz
-cp -a html/* /usr/share/nagios3/htdocs/
-cp -a html/stylesheets/* /etc/nagios3/stylesheets/.
-cd ..
-rm nagios-nuvola-1.0.3.tar.gz
-rm -rf nuvola/
-
-
-# Configure Nuvola
-sed -i -r 's#/nagios/#/nagios3/#' /usr/share/nagios3/htdocs/config.js
-mv /usr/share/nagios3/htdocs/side.html /usr/share/nagios3/htdocs/side.php
-
-# Create the Nagiosadmin user and set a password.
-htpasswd -cb /etc/nagios3/htpasswd.users nagiosadmin nagios
-
-# Reboot and begin configuration of Nagios.
+echo "Install Nagios 4"
+echo "================"
 echo
-echo "----------------------------------"
-echo "Reboot Raspberry Pi and begin using Nagios?"
-echo "See Nagios manual about configuring Nagios."
-read -p "Press y to reboot and n to exit" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    reboot
+echo " · ~70 MB space occupied."
+echo " · Once installed, this script can uninstall the app."
+echo " · Nagios is a popular open-source software that is designed to monitor systems, networks, and infrastructure."
+echo " · Installing on a modified system could overwrite any previous modifications."
+echo " · When finish and reboot, you can browser to $URL"
+echo
+read -p "Are you sure you want to continue? (Y/n) " response
+
+if [[ $response =~ [Nn] ]]; then
+    exit_message
 fi
+
+install
+post_install
