f355af05eb79cf2c1f18e55d079f5d1ab56fb2c8
cerrejon <ulysess@gmail.com>
2020-11-09 19:18:09 +0100

    👌 IMPROVE: Change DATA_URL

--- scripts/games/openbor.sh
+++ scripts/games/openbor.sh
@@ -2,7 +2,7 @@
 #
 # Description : OpenBOR
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.4.4 (06/Oct/20)
+# Version     : 1.4.5 (08/Nov/20)
 # Compatible  : Raspberry Pi 4 (tested)
 # Repository  : https://github.com/crcerror/OpenBOR-63xx-RetroPie-openbeta
 # Help		  : https://www.raspberrypi.org/forums/viewtopic.php?f=78&t=26859&start=25
@@ -19,7 +19,7 @@ PACKAGES=(libsdl-gfx1.2-5 libpng12-0 libsdl2-gfx-1.0-0 libvorbisidec1)
 PACKAGES_DEV=(libsdl2-gfx-dev libvorbisidec-dev libvpx-dev libogg-dev libsdl2-gfx-1.0-0 libvorbisidec1)
 BINARY_URL="https://misapuntesde.com/rpi_share/openbor_by_ulysess.tar.gz"
 GITHUB_URL="https://github.com/crcerror/OpenBOR-Raspberry"
-DATA_URL="https://www.dropbox.com/s/jonj7xla9y30u8f/SOR2X_V2.1.pak?dl=0"
+DATA_URL="https://e.pcloud.link/publink/show?code=XZe7X7Z3SUzAIxYOUJSJQ9OMvT83LwoxgrX"
 
 runme() {
     read -p "Press [ENTER] to run the game..."
