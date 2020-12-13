24b549563fecb2052724b476dd4abe8f2755fd14
Jose Cerrejon <ulysess@gmail.com>
2020-12-13 12:43:25 +0100

    👌 IMPROVE: Add SDL2_Net and refactor code

--- scripts/others/sdl2.sh
+++ scripts/others/sdl2.sh
@@ -2,7 +2,7 @@
 #
 # Description : Compile SDL 2.0.12
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.4.1 (28/Sep/20)
+# Version     : 1.4.3 (12/Dec/20)
 #
 # Help        : https://github.com/jlnr/gosu/wiki/Getting-Started-on-Raspbian-%28Raspberry-Pi%29
 # 	          : https://hg.libsdl.org/SDL/file/default/build-scripts/raspberrypi-buildbot.sh
@@ -11,36 +11,22 @@
 clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-SDL2_DBG_PKG_URL="https://malus.exotica.org.uk/~buzz/pi/sdl/sdl2/libsdl2-dbg_2.0.12_armhf.deb"
-SDL2_DEV_PKG_URL="http://ftp.br.debian.org/debian/pool/main/libs/libsdl2/libsdl2-dev_2.0.12+dfsg1-1_armhf.deb"
-SDL2_PKG_URL="http://ftp.br.debian.org/debian/pool/main/libs/libsdl2/libsdl2-2.0-0_2.0.12+dfsg1-1_armhf.deb"
 SDL2_SC_URL="https://www.libsdl.org/release/SDL2-2.0.12.zip"
 SDL2_TTF_SC_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.zip"
 SDL2_IMAGE_SC_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.zip"
 SDL2_MIXER_SC_URL="https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.zip"
-SDL2_ALT_URL="https://buildbot.libsdl.org/sdl-builds/sdl-raspberrypi/sdl-raspberrypi-114.tar.xz"
+SDL2_NET_SC_URL="https://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.1.zip"
+SDL2_ALT_URL="https://buildbot.libsdl.org/sdl-builds/sdl-raspberrypi/sdl-raspberrypi-438.tar.xz"
 
-Install_BIN_RPi1() {
+install_method_1() {
     clear
-    echo -e "\nInstalling SDL 2.0.12..."
+    echo -e "\nInstalling SDL 2.0.14..."
     wget -q -O /tmp/sdl2.tar.xz "$SDL2_ALT_URL"
     echo "Extracting..."
     cd /tmp && extract /tmp/sdl2.tar.xz
     echo "Moving..."
     sudo rsync -a --remove-source-files /tmp/usr /
     rm -rf /tmp/usr
-    ldconfig_and_show_version
-}
-
-Install_BIN_RPi2() {
-    clear
-    echo -e "\nDownloading 2.0.12..."
-    wget -P "$HOME" "$SDL2_DBG_PKG_URL"
-    wget -P "$HOME" "$SDL2_DEV_PKG_URL"
-    wget -P "$HOME" "$SDL2_PKG_URL"
-    echo -e "\nInstalling SDL..."
-    sudo dpkg -i "$HOME"/libsdl2*.deb
-    rm -f "$HOME"/"$(basename $SDL2_DBG_PKG_URL)" "$HOME"/"$(basename $SDL2_DEV_PKG_URL)" "$HOME"/"$(basename $SDL2_PKG_URL)"
     ldconfig_and_show_version
 }
 
@@ -57,7 +43,7 @@ Compile_SDL_RPi() {
     clear
     local INSTALL_GLOBALLY_AFTER_COMPILE=1
 
-    echo -e "Compile SDL 2.0.12 + ttf + image + mixer\n======================================\nIt can takes On Rpi 4 ~15 min. Be patience...\n"
+    echo -e "Compile SDL 2.0.12 + ttf + image + mixer + net\n======================================\nIt can takes On Rpi 4 ~15 min. Be patience...\n"
 
     read -p "Do you want to install globally each library after compilation (Y/n)? " response
     if [[ $response =~ [Nn] ]]; then
@@ -71,13 +57,14 @@ Compile_SDL_RPi() {
         libsmpeg-dev libjpeg-dev libsamplerate0-dev libjack-dev libopusfile-dev libmpg123-dev libfluidsynth-dev libesd0-dev libvulkan-dev \
         autoconf libvulkan-dev libvulkan1
 
-    mkdir -p "$HOME"/sc && cd "$_"
+    mkdir -p "$HOME"/sc && cd "$_" || return
 
     echo -e "\nDownloading source code...\n"
     wget -q --show-progress "$SDL2_SC_URL"
     wget -q --show-progress "$SDL2_TTF_SC_URL"
     wget -q --show-progress "$SDL2_IMAGE_SC_URL"
     wget -q --show-progress "$SDL2_MIXER_SC_URL"
+    wget -q --show-progress "$SDL2_NET_SC_URL"
 
     # Unzip them all!
     echo -e "\nUncompressing...\n"
@@ -86,29 +73,35 @@ Compile_SDL_RPi() {
     # Cleaning da house
     rm SDL2-2.*.zip SDL2_*.zip
 
-    cd SDL2-2.*
+    cd SDL2-2.* || return
     ./autogen.sh
     ./configure --disable-video-opengl --disable-video-x11 --disable-pulseaudio --disable-esd --enable-video-opengles --enable-libudev --enable-arm-simd --enable-video-vulkan --host=arm-raspberry-linux-gnueabihf
-    make -j"$(getconf _NPROCESSORS_ONLN)"
+    make_with_all_cores
     if [[ ${INSTALL_GLOBALLY_AFTER_COMPILE} -eq 1 ]]; then
         sudo make install
     fi
 
-    cd ../SDL2_ttf-2.*
+    cd ../SDL2_ttf-2.0.15 || return
     ./configure
-    make -j"$(getconf _NPROCESSORS_ONLN)"
+    make_with_all_cores
     if [[ ${INSTALL_GLOBALLY_AFTER_COMPILE} -eq 1 ]]; then
         sudo make install
     fi
-    cd ../SDL2_image-2*
+    cd ../SDL2_image-2.0.5 || return
     ./configure
-    make -j"$(getconf _NPROCESSORS_ONLN)"
+    make_with_all_cores
     if [[ ${INSTALL_GLOBALLY_AFTER_COMPILE} -eq 1 ]]; then
         sudo make install
     fi
-    cd ../SDL2_mixer-2*
+    cd ../SDL2_mixer-2.0.4 || return
     ./configure
-    make -j"$(getconf _NPROCESSORS_ONLN)"
+    make_with_all_cores
+    if [[ ${INSTALL_GLOBALLY_AFTER_COMPILE} -eq 1 ]]; then
+        sudo make install
+    fi
+    cd ../SDL2_net-2.0.1 || return
+    ./configure
+    make_with_all_cores
     if [[ ${INSTALL_GLOBALLY_AFTER_COMPILE} -eq 1 ]]; then
         sudo make install
     fi
@@ -120,9 +113,8 @@ Compile_SDL_RPi() {
 cmd=(dialog --clear --title "[ Compile SDL2 version ]" --menu "Select an option from the list. This process can't be undone:" 11 80 50)
 
 options=(
-    METHOD_1 "Install the binaries 2.0.11 (Recommended)"
-    # METHOD_2 "Install the binaries 2.0.12"
-    COMPILE "Compile SDL2 + ttf + image + mixer with Vulkan support"
+    METHOD_1 "Install the binaries 2.0.14 (Recommended)"
+    COMPILE "Compile SDL2 + ttf + image + mixer + net with Vulkan support"
     EXIT "Exit from the script"
 )
 
@@ -130,8 +122,7 @@ choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
 for choice in $choices; do
     case $choice in
-    METHOD_1) Install_BIN_RPi1 ;;
-    METHOD_2) Install_BIN_RPi2 ;;
+    METHOD_1) install_method_1 ;;
     COMPILE) Compile_SDL_RPi ;;
     EXIT) exit ;;
     esac
