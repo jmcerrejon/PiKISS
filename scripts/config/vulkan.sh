115b239209b8a7ee1f02d3ad30f72ddae9a05606
Jose Cerrejon <ulysess@gmail.com>
2020-12-15 11:23:23 +0100

    👌 IMPROVE: Freeze version to 20.3

--- scripts/config/vulkan.sh
+++ scripts/config/vulkan.sh
@@ -2,13 +2,13 @@
 #
 # Description : Vulkan driver
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1.3 (21/Nov/20)
+# Version     : 1.2.0 (15/Dec/20)
 # Compatible  : Raspberry Pi 4
 #
 # Info        : Thks to PI Labs
 # Help        : https://ninja-build.org/manual.html#ref_pool
 #             : https://www.raspberrypi.org/forums/viewtopic.php?f=63&t=276412&start=25#p1678723
-# 	          : https://blogs.igalia.com/apinheiro/2020/06/v3dv-quick-guide-to-build-and-run-some-demos/
+#             : https://blogs.igalia.com/apinheiro/2020/06/v3dv-quick-guide-to-build-and-run-some-demos/
 #             : https://github.com/Yours3lf/rpi-vk-driver/blob/master/BUILD.md
 #
 . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
@@ -16,12 +16,12 @@ clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 readonly INSTALL_DIR="$HOME/mesa_vulkan"
-readonly SOURCE_CODE_URL="git://anongit.freedesktop.org/mesa/mesa"
-readonly PI_VERSION_NUMBER=$(< /proc/device-tree/model awk '{print $3}')
+readonly SOURCE_CODE_URL="https://gitlab.freedesktop.org/mesa/mesa/-/tree/20.3"
+readonly PI_VERSION_NUMBER=$(awk </proc/device-tree/model '{print $3}')
 
 install() {
     echo -e "\nInstalling,...\n"
-    cd "$HOME"/mesa_vulkan
+    cd "$INSTALL_DIR" || exit
     sudo ninja -C build install
     echo
     glxinfo -B
@@ -57,34 +57,16 @@ install_full_deps() {
 
 clone_repo() {
     echo -e "\nCloning mesa repo...\n"
-    cd "$HOME"
-    git clone "$SOURCE_CODE_URL" mesa_vulkan && cd "$_"
-}
-
-update_repo() {
-    cd "$HOME"/mesa_vulkan
-    git fetch --all
-    git pull origin
+    cd || exit
+    git clone -b 20.3 https://gitlab.freedesktop.org/mesa/mesa.git "$INSTALL_DIR" && cd "$_" || exit
 }
 
 compile() {
     local EXTRA_PARAM
 
-    if [[ -d "$HOME"/mesa_vulkan ]]; then
-        echo
-        read -p "Directory exists. Do you want to update the repo & compile it again (y/N)? " response
-        if [[ $response =~ [Yy] ]]; then
-            echo -e "\nDownloading the latest changes...\n"
-            update_repo
-            ninja clean
-        else
-            exit_message
-            return 1
-        fi
-    else
-        install_full_deps
-        clone_repo
-    fi
+    [[ -d $INSTALL_DIR ]] && rm -rf "$INSTALL_DIR"
+    install_full_deps
+    clone_repo
 
     [[ -d "$INSTALL_DIR"/build ]] && rm -rf "$INSTALL_DIR"/build
 
@@ -93,13 +75,14 @@ compile() {
     fi
 
     # Check in a future the next params for better performance. It seems it's failing due some incompatible params.
-    # ... -Dgallium-drivers=v3d,kmsro,vc4,zink,virgl 
+    # ... -Dgallium-drivers=v3d,kmsro,vc4,zink,virgl
     meson --prefix /usr -Dgles1=disabled -Dgles2=enabled -Dplatforms=x11 -Dvulkan-drivers=broadcom -Ddri-drivers= -Dgallium-drivers=v3d,kmsro,vc4,virgl -Dbuildtype=release -Dc_args="$EXTRA_PARAM" -Dcpp_args="$EXTRA_PARAM" build
     echo -e "\nCompiling... Estimated time on Raspberry Pi 4 over USB/SSD drive (Not overclocked): ~12 min. \n"
     time ninja -C build -j"$(nproc)"
     install
 }
 
+install_script_message
 upgrade_dist
 compile
 exit_message
