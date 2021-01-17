59df23fb56d23d98730a2d6c861007dea874695a
Jose Cerrejon <ulysess@gmail.com>
2021-01-17 19:00:06 +0100

    👌 IMPROVE: Add Zendronum. Refactor code

--- scripts/games/cdoom.sh
+++ scripts/games/cdoom.sh
@@ -1,181 +1,228 @@
 #!/bin/bash
 #
-# Description : Crispy-Doom ver. 5.8.0 to play DOOM & Heretic
+# Description : Zandronum and Crispy-Doom
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.6.6 (23/Oct/20)
+# Version     : 2.0.0 (17/Jan/21)
 # Compatible  : Raspberry Pi 4 (tested)
 #
 # HELP        : To compile crispy-doom, follow the instructions at https://github.com/fabiangreffrath/crispy-doom
-# 	          : https://zdoom.org/wiki/Compile_GZDoom_on_Linux
-# TODO        : Refactor Code
 #
 . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-WAD_PATH="$HOME/games"
-PACKAGES=(timidity libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-mixer-2.0-0)
-PACKAGES_DEV=(build-essential automake git timidity libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-mixer-dev)
-PACKAGES_DEV_GZ_DOOM=( g++ make cmake libsdl2-dev git zlib1g-dev libbz2-dev libjpeg-dev libfluidsynth-dev libgme-dev libopenal-dev libmpg123-dev libsndfile1-dev libgtk-3-dev timidity nasm libgl1-mesa-dev tar libsdl1.2-dev libglew-dev)
-URL_DOOM="https://misapuntesde.com/rpi_share/dc.zip"
-URL_HERETIC="https://misapuntesde.com/rpi_share/hc.zip"
-URL_HEXEN="https://misapuntesde.com/rpi_share/hec.zip"
-URL_STRIFE="https://misapuntesde.com/rpi_share/sc.zip"
-CRISPY_DOOM="https://misapuntesde.com/rpi_share/crispy_5-8.0_armhf.deb"
-CRISPY_DOOM_SOURCE="https://github.com/fabiangreffrath/crispy-doom.git"
-GZ_DOOM_SOURCE="https://github.com/drfrag666/gzdoom.git"
-# CHOCOLATE_DOOM="https://misapuntesde.com/rpi_share/chocolate_3-0_armhf.deb" # Future release?
-LICENSE="Complete"
-IS_DOOM_INSTALLED=0
-IS_HERETIC_INSTALLED=0
+readonly INSTALL_DIR="$HOME/games"
+readonly PACKAGES=(timidity libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-mixer-2.0-0)
+readonly PACKAGES_ZANDRONUM=(libglu1-mesa)
+readonly PACKAGES_DEV_CRISPY=(build-essential automake git timidity libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-mixer-dev)
+readonly PACKAGES_DEV_GZ_DOOM=(g++ make cmake libsdl2-dev git zlib1g-dev libbz2-dev libjpeg-dev libfluidsynth-dev libgme-dev libopenal-dev libmpg123-dev libsndfile1-dev libgtk-3-dev timidity nasm libgl1-mesa-dev tar libsdl1.2-dev libglew-dev)
+readonly PACKAGES_DEV=(g++ make cmake libsdl2-dev git zlib1g-dev libbz2-dev libjpeg-dev libfluidsynth-dev libgme-dev libopenal-dev libmpg123-dev libsndfile1-dev libgtk-3-dev timidity nasm libgl1-mesa-dev tar libsdl1.2-dev libglew-dev libssl-dev)
+readonly ZANDRONUM_BINARY_URL="https://misapuntesde.com/rpi_share/zandronum-rpi.tar.gz"
+readonly ZANDRONUM_SOURCE_CODE_URL="https://github.com/ptitSeb/zandronum"
+readonly CRISPY_DOOM_PKG_URL="https://misapuntesde.com/rpi_share/crispy_5-8.0_armhf.deb"
+readonly CRISPY_DOOM_SOURCE="https://github.com/fabiangreffrath/crispy-doom.git"
+readonly GZ_DOOM_SOURCE="https://github.com/drfrag666/gzdoom.git"
+readonly # CHOCOLATE_DOOM="https://misapuntesde.com/rpi_share/chocolate_3-0_armhf.deb" # Future release?
+readonly VAR_DATA_NAME="WADS"
+DATA_URL="https://misapuntesde.com/rpi_share/wads-shareware.tar.gz"
+INPUT=/tmp/doom.$$
 
 runme() {
-    if [ -e /usr/bin/doom ]; then
-        read -p "Do you want to play Doom right now (y/N)? " response
-        if [[ $response =~ [Yy] ]]; then
-            doom
-        fi
-    else
-        if [ -e /usr/bin/heretic ]; then
-            read -p "Do you want to play Heretic right now (y/N)? " response
-            if [[ $response =~ [Yy] ]]; then
-                heretic
-            fi
-        fi
+    [[ ! -e $INSTALL_DIR/zandronum/zandronum ]] && exit_message
+
+    read -p "Do you want to play now (Y/n)? " response
+    if [[ $response =~ [Nn] ]]; then
+        exit_message
     fi
+    cd "$INSTALL_DIR/zandronum" && ./zandronum
     exit_message
 }
 
 removeUnusedLinks() {
     rm -f ~/.local/share/applications/crispy-hexen.desktop ~/.local/share/applications/crispy-strife.desktop
     rm -f /usr/local/share/applications/io.github.fabiangreffrath.Doom.desktop /usr/local/share/applications/io.github.fabiangreffrath.Heretic.desktop /usr/local/share/applications/io.github.fabiangreffrath.Setup.desktop
-    if [[ $IS_DOOM_INSTALLED -eq 0 ]]; then
-        rm -f ~/.local/share/applications/crispy-doom.desktop
-    fi
-    if [[ $IS_HERETIC_INSTALLED -eq 0 ]]; then
-        rm -f ~/.local/share/applications/crispy-heretic.desktop
-    fi
+    rm -f ~/.local/share/applications/crispy-doom.desktop
+    rm -f ~/.local/share/applications/crispy-heretic.desktop
 }
 
 remove_files() {
-    rm -rf "$WAD_PATH"/wads
-    rm -f ~/.local/share/applications/crispy-doom.desktop ~/.local/share/applications/crispy-heretic.desktop
+    rm -rf "$INSTALL_DIR"/wads
     sudo rm -rf /usr/bin/doom /usr/bin/heretic
+    removeUnusedLinks
 }
 
-uninstall() {
-    read -p "Do you want to uninstall Crispy Doom (y/N)? " response
+uninstall_crispy_doom() {
+    read -p "Do you want to uninstall Crispy-Doom (y/N)? " response
     if [[ $response =~ [Yy] ]]; then
         sudo apt remove -y crispy
         remove_files
-        if [[ -e "$INSTALL_DIR"/arx ]]; then
+        if [[ -e /usr/local/bin/crispy-doom ]]; then
             echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
             exit_message
         fi
         echo -e "\nSuccessfully uninstalled."
         exit_message
     fi
-    runme
 }
 
-if [ -e "/usr/local/bin/crispy-doom" ]; then
-    echo -e "Crispy-Doom already installed!."
-    uninstall
-    exit 1
-fi
-
-share_version() {
-    URL_DOOM="https://misapuntesde.com/rpi_share/ds.zip"
-    URL_HERETIC="https://misapuntesde.com/rpi_share/hs.zip"
-    URL_HEXEN="https://misapuntesde.com/rpi_share/hes.zip"
-    URL_STRIFE="https://misapuntesde.com/rpi_share/ss.zip"
+uninstall_zandronum() {
+    read -p "Do you want to uninstall Zandronum (y/N)? " response
+    if [[ $response =~ [Yy] ]]; then
+        rm -rf "$INSTALL_DIR/zandronum" ~/.local/share/applications/zandronum.desktop
+        if [[ -e $INSTALL_DIR/zandronum ]]; then
+            echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
+            exit_message
+        fi
+        echo -e "\nSuccessfully uninstalled."
+        exit_message
+    fi
 }
 
-compile() {
-    install_packages_if_missing "${PACKAGES_DEV[@]}"
-    cd "$WAD_PATH"
-    git clone "$CRISPY_DOOM_SOURCE" crispy-doom && cd "$_"
+download_data_files() {
+    echo -e "\nInstalling data files (Shareware WADs)..."
+    if [[ $(extract_path_from_file "$VAR_DATA_NAME") != '' ]]; then
+        DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME")
+    fi
+    download_and_extract "$DATA_URL" "$1"
+}
+
+compile_crispy_doom() {
+    install_packages_if_missing "${PACKAGES_DEV_CRISPY[@]}"
+    mkdir -p "$HOME/sc" && cd "$_" || exit 1
+    git clone "$CRISPY_DOOM_SOURCE" crispy-doom && cd "$_" || exit 1
     sudo apt build-dep crispy-doom
     autoreconf -fiv
     ./configure
-    echo -e "\nCompiling...\n"
-    make -j"$(getconf _NPROCESSORS_ONLN)"
+    make_with_all_cores
     echo -e "\nDone! . Go to $(pwd) to run the binaries or type make install to install the app.\n"
+}
+
+generate_icon_crispy_doom() {
+    echo -e "\nGenerating icon..."
+    if [[ ! -e ~/.local/share/applications/crispy-doom.desktop ]]; then
+        cat <<EOF >~/.local/share/applications/crispy-doom.desktop
+[Desktop Entry]
+Name=Crispy Doom
+Version=1.0
+Type=Application
+Comment=Limit-removing enhanced-resolution Doom source port
+Exec=crispy-doom -iwad ${INSTALL_DIR}/wads/DOOM.WAD
+Icon=crispy-doom
+Terminal=false
+Categories=Game;ActionGame;
+EOF
+    fi
+    if [[ ! -e ~/.local/share/applications/crispy-heretic.desktop ]]; then
+        cat <<EOF >~/.local/share/applications/crispy-heretic.desktop
+[Desktop Entry]
+Name=Crispy Heretic
+Version=1.0
+Type=Application
+Comment=Limit-removing enhanced-resolution Doom source port
+Exec=crispy-heretic -iwad ${INSTALL_DIR}/wads/HERETIC.WAD
+Icon=crispy-doom
+Terminal=false
+Categories=Game;ActionGame;
+EOF
+    fi
+}
+
+install_crispy() {
+    if [[ -e /usr/local/bin/crispy-doom ]]; then
+        clear
+        echo -e "Crispy-Doom already installed!."
+        uninstall_crispy_doom
+        exit 1
+    fi
+    local SHORTCUT_URL
+    SHORTCUT_URL="https://misapuntesde.com/res/crispy_modified_link.zip"
+    install_script_message
+    install_packages_if_missing "${PACKAGES[@]}"
+    download_and_install "$CRISPY_DOOM_PKG_URL"
+    removeUnusedLinks
+    generate_icon_crispy_doom
+    download_data_files "$INSTALL_DIR/wads"
+    echo -e "\nYou can play typing crispy-doom <WAD_FILE> or opening the Menu > Games > Doom or Heretic."
+    exit_message
 }
 
 compile_gzdoom() {
     install_packages_if_missing "${PACKAGES_DEV_GZ_DOOM[@]}"
-    mkdir -p "$HOME/sc"
-    git clone "$GZ_DOOM_SOURCE" gzdoom && cd "$_"
+    mkdir -p "$HOME/sc" && cd "$_" || exit 1
+    git clone "$GZ_DOOM_SOURCE" gzdoom && cd "$_" || exit 1
     wget -nc http://zdoom.org/files/fmod/fmodapi44464linux.tar.gz && tar -xvzf fmodapi44464linux.tar.gz -C .
-    mkdir -pv build && cd "$_"
+    mkdir -pv build && cd "$_" || exit 1
     cmake .. -DNO_FMOD=ON
 }
 
-install() {
-    [ ! -d "$HOME"/games ] && mkdir -p "$HOME"/games
-    install_packages_if_missing "${PACKAGES[@]}"
-    wget -O "$HOME"/crispy-doom.deb "$CRISPY_DOOM"
-    sudo dpkg -i "$HOME"/crispy-doom.deb
-    rm "$HOME"/crispy-doom.deb
-    wget -P ~/.local/share/applications https://misapuntesde.com/res/crispy_modified_link.zip && unzip -q -o ~/.local/share/applications/crispy_modified_link.zip -d ~/.local/share/applications && rm ~/.local/share/applications/crispy_modified_link.zip
+generate_icon_zandronum() {
+    echo -e "\nGenerating icon..."
+    if [[ ! -e ~/.local/share/applications/zandronum.desktop ]]; then
+        cat <<EOF >~/.local/share/applications/zandronum.desktop
+[Desktop Entry]
+Name=Zandronum
+Version=1.0
+Type=Application
+Comment=Zandronum is a multiplayer oriented port, based off Skulltag, for Doom I/II and derivates
+Exec=${INSTALL_DIR}/zandronum/zandronum
+Icon=${INSTALL_DIR}/zandronum/icon.png
+Path=${INSTALL_DIR}/zandronum/
+Terminal=false
+Categories=Game;
+EOF
+    fi
+}
+
+compile_zandronum() {
+    install_packages_if_missing "${PACKAGES_DEV[@]}"
+    mkdir -p "$HOME/sc" && cd "$_" || exit 1
+    git clone --recursive "$GITHUB_URL" zandronum && cd "$_" || exit 1
+    mkdir build && cd "$_" || exit 1
+    # TODO Returning valid CFLAGS for different RPi, Check https://github.com/ptitSeb/zandronum/blob/master/CMakeLists.txt
+    CFLAGS="-fsigned-char -marm -march=armv8-a+crc -mtune=cortex-a72 -mfpu=neon-fp-armv8 -mfloat-abi=hard" CXXFLAGS="-fsigned-char" cmake .. -DNO_FMOD=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -Wno-dev
+    make_with_all_cores "\nCompiling..."
+    read -p "Do you want to install globally (y/N)? " response
+    if [[ $response =~ [Yy] ]]; then
+        sudo make install
+    fi
+    echo -e "\nDone!. Check the code at $HOME/sc/MY_APP."
+    exit_message
+}
+
+install_zandronum() {
+    if [[ -e $INSTALL_DIR/zandronum/zandronum ]]; then
+        clear
+        echo -e "Zandronum already installed!."
+        uninstall_zandronum
+        exit 1
+    fi
+    install_script_message
+    install_packages_if_missing "${PACKAGES_ZANDRONUM[@]}"
+    download_and_extract "$ZANDRONUM_BINARY_URL" "$INSTALL_DIR"
+    generate_icon_zandronum
+
+    download_data_files "$INSTALL_DIR/zandronum"
+    echo -e "\nYou can play typing $INSTALL_DIR/zandronum/zandronum or opening the Menu > Games > Zandronum."
+    runme
 }
 
 menu() {
-    clear
-    echo
-    read -p "Do you have data files set on the file res/magic-air-copy-pikiss.txt for Doom/Heretic (If not, a Shareware version will be installed) (Y/n)?: " response
-    if [[ $response =~ [Nn] ]]; then
-        share_version
-        LICENSE="Shareware"
-    fi
+    while true; do
+        dialog --clear \
+            --title "[ DOOM'S ENGINE ]" \
+            --menu "Select from the list:" 11 100 3 \
+            ZANDRONUM "(Recommended) Multiplayer oriented port for Doom I/II,Heretic,Hexen,Strife..." \
+            CRISPY_DOOM "Another engine with Doom + Heretic support" \
+            Exit "Exit" 2>"${INPUT}"
 
-    if [[ $LICENSE = "Complete" ]]; then
-        message_magic_air_copy "$URL_DOOM"
-    fi
+        menuitem=$(<"${INPUT}")
 
-    cmd=(dialog --separate-output --title "[ Crispy-Doom. WADs License: $LICENSE ]" --checklist "Move with the arrows up & down. Space to select the game(s) you want to install" 13 120 16)
-    options=(
-        Doom "Space marine operating under the UAC (Union Aerospace Corporation), who fights hordes of demons" on
-        Heretic "Player must first fight through the undead hordes infesting the site" off
-        #  Hexen "It is the sequel to 1994's Heretic" off
-        #  Strife "The game is set in a world where a dark religion called The Order has taken over" off
-    )
-    choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-
-    for choice in $choices; do
-        case $choice in
-        Doom)
-            clear
-            download_and_extract "$URL_DOOM" "$WAD_PATH"
-            sudo sh -c "echo 'crispy-doom -iwad $WAD_PATH/wads/DOOM.WAD' > /usr/bin/doom" && sudo chmod +x /usr/bin/doom
-            IS_DOOM_INSTALLED=1
-            ;;
-        Heretic)
-            clear
-            download_and_extract "$URL_HERETIC" "$WAD_PATH"
-            sudo sh -c "echo 'crispy-heretic -iwad $WAD_PATH/wads/HERETIC.WAD' > /usr/bin/heretic" && sudo chmod +x /usr/bin/heretic
-            IS_HERETIC_INSTALLED=1
-            ;;
-        Hexen)
-            clear
-            download_and_extract "$URL_HEXEN" "$WAD_PATH"
-            sudo sh -c "echo 'crispy-hexen -iwad $WAD_PATH/wads/HEXEN.WAD' > /usr/bin/hexen" && sudo chmod +x /usr/bin/hexen
-            ;;
-        Strife)
-            clear
-            download_and_extract "$URL_STRIFE" "$WAD_PATH"
-            sudo sh -c "echo 'crispy-strife -iwad $WAD_PATH/wads/STRIFE.WAD' > /usr/bin/strife" && sudo chmod +x /usr/bin/strife
-            ;;
+        case $menuitem in
+        ZANDRONUM) clear && install_zandronum ;;
+        CRISPY_DOOM) clear && install_crispy ;;
+        Exit) exit ;;
         esac
     done
 }
 
-install
 menu
-removeUnusedLinks
-
-echo -e "Installed. To play, just type doom, heretic or Go to Menu > Games.\n"
-runme
-echo
-read -p "Press [Enter] to go back to the menu..."
