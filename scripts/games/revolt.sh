9ca3ee077abfd9480bebcec7befcec8cb8482e6b
Jose Cerrejon <ulysess@gmail.com>
2021-02-07 17:11:04 +0100

    👌 IMPROVE: Rename function fix_libGLES

--- scripts/games/revolt.sh
+++ scripts/games/revolt.sh
@@ -2,7 +2,7 @@
 #
 # Description : Re-Volt is a radio control car racing themed video game.
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0.2 (17/Aug/20)
+# Version     : 1.0.3 (07/Feb/21)
 # Compatible  : Raspberry Pi 3-4 (tested on Raspberry Pi 4)
 #
 # HELP	      : Thanks to PI LAB (https://www.youtube.com/channel/UCgfQjdc5RceRlTGfuthBs7g) and Meverick
@@ -14,16 +14,16 @@ check_board || { echo "Missing file helper.sh. I've tried to download it for you
 DATA_PATH="https://misapuntesde.com/rpi_share/rvgl-data.deb"
 APP_PATH="https://misapuntesde.com/rpi_share/rvgl.deb"
 
-installer(){
-	sudo apt install -y libsdl2-image-2.0-0 libenet7 libunistring-dev
-	fixlibGLES
-	cd ~
-	if [ ! -f /usr/local/bin/rvgl_start ]; then
-		wget -O rvgl-data.deb $DATA_PATH
-		wget -O rvgl.deb $APP_PATH
-		sudo dpkg -i rvgl-data.deb rvgl.deb
-		rm rvgl-data.deb rvgl.deb
-	fi
+installer() {
+    sudo apt install -y libsdl2-image-2.0-0 libenet7 libunistring-dev
+    fix_libGLES
+    cd ~
+    if [ ! -f /usr/local/bin/rvgl_start ]; then
+        wget -O rvgl-data.deb $DATA_PATH
+        wget -O rvgl.deb $APP_PATH
+        sudo dpkg -i rvgl-data.deb rvgl.deb
+        rm rvgl-data.deb rvgl.deb
+    fi
 }
 
 echo -e "Installing Re-Volt...\n=====================\n\n· Please wait...\n"
