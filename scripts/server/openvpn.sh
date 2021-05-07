33fceb521bb3ea14bdb23f587ac167a4860ae8f4
Jose Cerrejon <ulysess@gmail.com>
2021-05-07 19:38:02 +0200

    👌 IMPROVE: Refactor code. Add execution bit

--- scripts/server/openvpn.sh
+++ scripts/server/openvpn.sh
@@ -2,14 +2,24 @@
 #
 # Description : OpenVPN
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0 (21/Jul/2017)
+# Version     : 1.0.1 (7/May/2021)
 #
 # HELP		  : https://www.pivpn.io/
 #
+. ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 clear
 
-echo -e "Turn Raspberry Pi into a OpenVPN Server\n\nInstalling, please wait..."
+echo "
+PiVPN
+=====
+
+· Turn Raspberry Pi into a OpenVPN Server
+· More info: https://www.pivpn.io/
+"
+
+read -p 'Press [ENTER] to continue...'
 
 curl -L https://install.pivpn.io | bash
 
-read -p 'Done. Press [ENTER] to continue...'
+exit_message
