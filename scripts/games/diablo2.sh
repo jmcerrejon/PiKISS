8783577c3fa5ab0ddee84d48de8da4b29bfa25c2
Jose Cerrejon <ulysess@gmail.com>
2021-08-28 08:58:14 +0200

    👌 IMPROVE: Check missing files. Refactor code

--- scripts/games/diablo2.sh
+++ scripts/games/diablo2.sh
@@ -2,7 +2,7 @@
 #
 # Description : Diablo 2 Exp. Spanish for Raspberry Pi
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1.7 (20/Jun/21)
+# Version     : 1.1.8 (28/Aug/21)
 # Compatible  : Raspberry Pi 4 (tested)
 #
 # Info		  : Thks to PI Labs and Notaz
@@ -14,135 +14,105 @@ clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 readonly INSTALL_DIR="$HOME/games"
+readonly PACKAGES=(wine)
 readonly SCRIPT_PATH="$HOME/games/diablo2/diablo2.sh"
-readonly BINARY_PATH="https://notaz.gp2x.de/misc/starec/libd2game_sa_arm.exe.so.xz"
-readonly VAR_DATA_NAME_EN="DIABLO2_EN"
-readonly VAR_DATA_NAME_ES="DIABLO2_ES"
-readonly PIKISS_PATH=$(pwd)
-INPUT=/tmp/diablo2.$$
-DATA_URL=""
+readonly BINARY_PATH="https://misapuntesde.com/rpi_share/diablo2-rpi.tar.gz"
+readonly VAR_DATA_NAME="DIABLO_2"
 
 remove_files() {
     rm -rf "$INSTALL_DIR"/diablo2 ~/.local/share/applications/diablo2.desktop
+}
+
+remove_wine() {
+    read -p "Do you want to remove wine? (y/N) " response
+    if [[ $response =~ [Yy] ]]; then
+        sudo apt remove -y wine
+    fi
 }
 
 uninstall() {
     read -p "Do you want to uninstall Diablo 2 (y/N)? " response
     if [[ $response =~ [Yy] ]]; then
         remove_files
+        remove_wine
         if [[ -e "$INSTALL_DIR"/diablo2 ]]; then
             echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
             exit_message
         fi
-        echo -e "\nSuccessfully uninstalled. NOTE: You need to uninstall wine manually with sudo apt remove -y wine"
+        echo -e "\nSuccessfully uninstalled."
         exit_message
     fi
     exit_message
 }
 
+if [[ -f /usr/local/bin/twistver ]]; then
+    echo "It's not recommended to install Diablo II on Twister OS at this time due to conflicts with x86 wine."
+    exit_message
+fi
+
+if [[ -d $INSTALL_DIR/diablo2 ]]; then
+    echo -e "Diablo 2 already installed.\n"
+    uninstall
+    exit 1
+fi
+
 generate_icon() {
     echo -e "\nGenerating icon..."
-    cp -f "$PIKISS_PATH"/res/diabloII.png "$INSTALL_DIR"/diablo2/diabloII.png
     if [[ ! -e ~/.local/share/applications/diablo2.desktop ]]; then
         cat <<EOF >~/.local/share/applications/diablo2.desktop
 [Desktop Entry]
 Name=Diablo 2 Lord of Destruction
 Exec=${INSTALL_DIR}/diablo2/diablo2.sh
+Path=${INSTALL_DIR}/diablo2/
 Icon=${INSTALL_DIR}/diablo2/diabloII.png
 Type=Application
+Terminal=true
 Comment=Set in the fictional Kingdom of Khanduras in the mortal realm, Diablo makes the player take control of a lone hero battling to rid the world of Diablo
 Categories=Game;ActionGame;
 EOF
     fi
 }
 
-copy_run_script() {
-    mkdir -p "$INSTALL_DIR"/diablo2
-    cp -f "$PIKISS_PATH"/res/diablo2.sh "$SCRIPT_PATH"
-}
-
-install_dependencies() {
-    echo -e "\nInstalling dependencies (if proceed)...\n"
-    if ! isPackageInstalled wine; then
-        sudo apt install -y wine
-    fi
-}
-
-download_binaries() {
-    echo -e "\nInstalling binary files..."
-    download_and_extract "$BINARY_PATH" "$INSTALL_DIR"/diablo2
-}
-
 end_message() {
     winecfg >/dev/null &
-    echo -e "\nOn winecfg, go to Graphics Tab and set Emulate a virtual desktop to 800x600. Then, run $SCRIPT_PATH or click on Menu > Games > Diablo 2 Lord of Destruction."
+    echo -e "\nDone. On winecfg, go to Graphics Tab and set Emulate a virtual desktop to 800x600. Then, run $SCRIPT_PATH or click on Menu > Games > Diablo 2 Lord of Destruction."
     exit_message
 }
 
 download_data_files() {
-    if ! exists_magic_file; then
-        echo -e "\nNow copy data directory into $INSTALL_DIR/diablo2."
-        end_message
+    DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME")
+    if [[ $DATA_URL == "" ]]; then
+        false
+        return
     fi
-    mkdir -p "$INSTALL_DIR" && cd "$_" || exit 1
+    message_magic_air_copy "$DATA_URL"
     download_and_extract "$DATA_URL" "$INSTALL_DIR"
-    end_message
-}
-
-choose_data_files() {
-    while true; do
-        dialog --clear \
-            --title "[ Diablo 2 Data files ]" \
-            --menu "Choose language:" 11 68 3 \
-            English "Install the game with English text and voices." \
-            Spanish "Install the game with Spanish text and voices." \
-            Exit "Exit" 2>"${INPUT}"
-
-        menuitem=$(<"${INPUT}")
-
-        case $menuitem in
-        English) clear && DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME_EN") && download_data_files ;;
-        Spanish) clear && DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME_ES") && download_data_files ;;
-        Exit) remove_files && clear && exit_message ;;
-        esac
-    done
+    true
+    return
 }
 
 install() {
-    mkdir -p "$INSTALL_DIR"/diablo2
-    install_dependencies
-    installMesa
-    copy_run_script
+    install_packages_if_missing "${PACKAGES[@]}"
+    download_and_extract "$BINARY_PATH" "$INSTALL_DIR"
+    install_mesa
     generate_icon
-    echo
-    read -p "Do you have data files set on the file res/magic-air-copy-pikiss.txt for Diablo 2 (Y/n)? " response
-    if [[ $response =~ [Nn] ]]; then
-        download_binaries
-        echo -e "\nDone. Please, copy Diablo2 file games inside $INSTALL_DIR/diablo2"
+    if exists_magic_file && download_data_files; then
+        end_message
+    else
+        echo -e "\nDone. Please, copy Diablo 2 file games inside $INSTALL_DIR/diablo2"
         end_message
     fi
-
-    choose_data_files
 }
-
-if [ -f "/usr/local/bin/twistver" ]; then
-    echo "It's not recommended to install Diablo II on Twister OS at this time due to conflicts with x86 wine."
-    exit_message
-fi
-
-if [[ -d "$INSTALL_DIR"/diablo2 ]]; then
-    echo -e "Diablo 2 already installed.\n"
-    uninstall
-    exit 1
-fi
 
 install_script_message
 echo "
-Install Diablo 2 thks to Notaz
-==============================
- · Languages: English, Spanish.
+Diablo 2
+========
+
+ · Thks to Notaz.
+ · It uses Wine.
  · Install path: $INSTALL_DIR/diablo2
- · NOTE: Only runs on Raspberry Pi 4
+ · NOTE: It runs on Raspberry Pi 4 only.
 "
 
 read -p "Press [Enter] to continue or [CTRL]+C to abort..."
