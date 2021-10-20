d32f144366279b3c09abfd8552e9c34e5b904234
Jose Cerrejon <ulysess@gmail.com>
2021-10-20 11:29:55 +0200

    👌 IMPROVE: WordPress, Node, Ghost.

--- scripts/server/fwork.sh
+++ scripts/server/fwork.sh
@@ -2,86 +2,130 @@
 #
 # Description : Install a Framework,CMS to the web server
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.7.3 (21/Jul/17)
+# Version     : 1.0.0 (20/Oct/21)
 #
-# Help        · Wordpress: https://github.com/raspberrypi/documentation/blob/master/usage/wordpress.md
-#             · PyPlate: https://pplware.sapo.pt/linux/dica-como-ter-o-seu-proprio-site-no-raspberry-pi/
+# Help
 #             · https://darryldias.me/wordpress-with-sqlite/
 #             · https://docs.ghost.org/docs/installing-ghost-via-the-cli#pre-requisites
+#             · https://www.nginx.com/resources/wiki/start/topics/recipes/wordpress/
+#             · https://user-meta.com/blog/light-weight-install-wordpress-sqlite/
+# TODO        · Uninstall Nginx (WordPress), Open Browser & VSCode
 #
 clear
 
 . ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-URL_GHOST="https://github.com/TryGhost/Ghost/releases/download/0.11.11/Ghost-0.11.11.zip"
-URL_WORDPRESS="https://wordpress.org/latest.tar.gz"
-WWW_PATH="/var/www/html"
+readonly LATEST_WORDPRESS_URL="https://wordpress.org/latest.tar.gz"
+readonly WWW_PATH="/var/www/html"
+readonly GHOST_REPOSITORY_NAME="TryGhost/Ghost"
+readonly WP_INSTALL_PATH="${WWW_PATH}/wordpress"
+readonly DEFAULT_NGINX_SITE_CONFIG_FILE_PATH="/etc/nginx/sites-available/default"
+DEFAULT_NGINX_SITES_AVAILABLE_DIR="/etc/nginx/sites-available"
+IP=$(get_ip)
 tempfile=$(mktemp)
 
-sqlite_integration(){
-  WP_INSTALL="${WWW_PATH}/wordpress"
-  USER_DB="$HOME/sc/db_wordpress"
-  wget -P $WP_INSTALL/wp-content/plugins/ https://downloads.wordpress.org/plugin/sqlite-integration.1.8.1.zip
-  cd $WP_INSTALL/wp-content/plugins || exit
-  sudo apt install -y sqlite3 php7.0-sqlite3
-  unzip $WP_INSTALL/wp-content/plugins/sqlite*.zip
-  cp $WP_INSTALL/wp-content/plugins/sqlite-integration/db.php $WP_INSTALL/wp-content/db.php
-  cp $WP_INSTALL/wp-config-sample.php $WP_INSTALL/wp-config.php
-  mkdir -p $USER_DB
-  echo "define('DB_FILE', 'wordpress');" >> $WP_INSTALL/wp-config.php
-  echo "define('DB_DIR', '${USER_DB}'); " >> $WP_INSTALL/wp-config.php
-  echo -e "SQLite integration installed.\n"
+wordpress_uninstall() {
+    [[ -d $WP_INSTALL_PATH ]] && sudo rm -rf $WP_INSTALL_PATH
+    uninstall_mariadb
+
+    echo
+    read -p "Do you want to remove Nginx and PHP from your system? (Y/n) " response
+    if [[ $response =~ [Yy] ]]; then
+        sudo apt remove -y nginx-light php*
+    fi
+
+    echo -e "\nUninstalled. It recommended to restart."
 }
 
-wordpress(){
-  cd "${WWW_PATH}" || return
-  sudo chown $USER: .
-  wget $URL_WORDPRESS
-  tar xzf ${URL_WORDPRESS##*/}
-  rm ${URL_WORDPRESS##*/}
-  sudo find "${WWW_PATH}" -type d -exec chmod 755 {} \;
-  sudo find "${WWW_PATH}" -type f -exec chmod 644 {} \;
-  read -p "Enable SQLite integration (not tested)? [y/n] " option
-  case "$option" in
-    y*) sqlite_integration ;;
-  esac
-  read -p "Wordpress installed on /var/www/html/wordpress directory. Press [Enter] to continue..."
+copy_default_wordpress_site() {
+    echo -e "\nCopying default site from $PIKISS_DIR/res/default.nginx to $DEFAULT_NGINX_SITE_CONFIG_FILE_PATH..."
+    if [[ -w $DEFAULT_NGINX_SITES_AVAILABLE_DIR ]]; then
+        mv "/etc/nginx/sites-available/default" "/etc/nginx/sites-available/default.bak"
+        cp "/home/pi/pikiss/res/default.nginx" "/etc/nginx/sites-available/default"
+        mv "$DEFAULT_NGINX_SITE_CONFIG_FILE_PATH" "$DEFAULT_NGINX_SITE_CONFIG_FILE_PATH.bak"
+        cp "$PIKISS_DIR/res/default.nginx" "$DEFAULT_NGINX_SITE_CONFIG_FILE_PATH"
+    else
+        echo -e "ERROR: Could not copy the file. You need to setting up Nginx manually or copy the file by yourself."
+    fi
+    sudo systemctl restart nginx
 }
 
-nodejs(){
-  install_node
-  read -p "Done!. Press [Enter] to continue..."
+wordpress() {
+    local WP_SITE
+    local WP_SITE_LOCAL
+    WP_SITE="http://${IP}/wordpress"
+    WP_SITE_LOCAL="http://127.0.0.1/wordpress"
+
+    if [[ -e $WP_INSTALL_PATH ]]; then
+        read -p "WordPress site already installed. Do you want to delete it? (Y/n) " response
+        if [[ $response =~ [Nn] ]]; then
+            exit_message
+        fi
+
+        wordpress_uninstall
+        exit_message
+    fi
+
+    install_script_message
+    echo -e "\nInstalling WordPress and dependencies...\n"
+    install_php 7
+    install_nginx
+    install_mariadb
+    copy_default_wordpress_site
+
+    echo -e "Searching latest WordPress installation files ...\n"
+    download_and_extract "$LATEST_WORDPRESS_URL" "${WWW_PATH}"
+
+    wordpress_post_install
+    open_default_browser "$WP_SITE_LOCAL"
+
+    echo -e "\nDone!. Wordpress installed at $WP_INSTALL_PATH. You can access through the following URL\nYour Pi: $WP_SITE_LOCAL\nRemote local computer: $WP_SITE"
+    exit_message
 }
 
-ghost(){
-  install_node 6
-  sudo mkdir -p "${WWW_PATH}" && cd $_ || return
-  sudo chown $USER: .
-  download_and_extract $URL_GHOST ./
-  cd "${WWW_PATH}/ghost" || exit
-  echo -e "\nInstalling Ghost, please wait...\n"
-  sudo npm install --production --unsafe-perm
-  sudo npm start
-  read -p "Done!. Press [Enter] to continue..."
+wordpress_post_install() {
+    sudo mysql -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO '${USER}'@'%'; FLUSH PRIVILEGES;"
+    sudo chown -R www-data: "$WP_INSTALL_PATH"
+    sudo find "${WWW_PATH}" -type d -exec chmod 755 {} \;
+    sudo find "${WWW_PATH}" -type f -exec chmod 644 {} \;
 }
 
-while true
-do
-  dialog --backtitle "PiKISS" --title "[ Install Framework ]" --clear --menu  "Pick one:" 15 55 6 \
-  Wordpress  "Wordpress (Latest)" \
-  Node  "Node.js (You choose)" \
-  Ghost  "Ghost (0.11.11)" \
-  Exit   "Exit" 2>"${tempfile}"
+nodejs() {
+    install_node
+    exit_message
+}
 
-  menuitem=$(<"${tempfile}")
-  clear
-  case $menuitem in
+ghost() {
+    local GHOST_LATEST_RELEASE_VERSION
+    GHOST_LATEST_RELEASE_VERSION=$(get_latest_release $GHOST_REPOSITORY_NAME)
+    local GHOST_LATEST_RELEASE_URL="https://github.com/TryGhost/Ghost/releases/download/$GHOST_LATEST_RELEASE_VERSION/Ghost-$GHOST_LATEST_RELEASE_VERSION.zip"
+    install_node 14
+    sudo mkdir -p "${WWW_PATH}" && cd "$_" || exit 1
+    sudo chown "$USER:" .
+    download_and_extract "$GHOST_LATEST_RELEASE_URL" "${WWW_PATH}"
+    cd "${WWW_PATH}/ghost" || exit 1
+    echo -e "\nInstalling Ghost, please wait...\n"
+    sudo npm install --production --unsafe-perm
+    sudo npm start
+    read -p "Done!. Press [Enter] to continue..."
+}
+
+while true; do
+    dialog --backtitle "PiKISS" --title "[ Install Framework ]" --clear --menu "Pick one:" 15 55 6 \
+        Wordpress "Nginx+PHP 7.4+MariaDB (Latest)" \
+        Node "Choose version" \
+        Ghost "Ghost (Latest)" \
+        Exit "Exit" 2>"${tempfile}"
+
+    menuitem=$(<"${tempfile}")
+    clear
+    case $menuitem in
     Wordpress) wordpress ;;
     Node) nodejs ;;
     Ghost) ghost ;;
     Exit) exit ;;
-  esac
+    esac
 done
 
-rm $tempfile
+rm "$tempfile"
