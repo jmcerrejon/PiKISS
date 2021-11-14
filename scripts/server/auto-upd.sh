6f1490f837341b5435d0a4392204aefdcf6be5ea
Jose Cerrejon <ulysess@gmail.com>
2021-11-14 17:16:25 +0100

    👌 IMPROVE: Refactor code

--- scripts/server/auto-upd.sh
+++ scripts/server/auto-upd.sh
@@ -2,7 +2,7 @@
 #
 # Description : keep Debian patched with latest security updates automatically
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0 (21/Jul/2017)
+# Version     : 1.1.0 (11/Nov/2021)
 #
 # HELP		  : https://www.cyberciti.biz/faq/how-to-keep-debian-linux-patched-with-latest-security-updates-automatically/
 #
@@ -10,31 +10,35 @@ clear
 . ../helper.sh || . ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-sudo apt install -y unattended-upgrades apt-listchanges bsd-mailx
-# file /etc/apt/apt.conf.d/50unattended-upgrades
-#      "o=Raspbian,n=buster";
-#      "o=Raspbian,a=stable";
+CODENAME=$(get_codename)
+PACKAGES=(unattended-upgrades apt-listchanges bsd-mailx)
 
-file_backup /etc/apt/apt.conf.d/50unattended-upgrades
+install() {
+    echo -e "\nInstalling..."
+    install_packages_if_missing "${PACKAGES[@]}"
+    file_backup /etc/apt/apt.conf.d/50unattended-upgrades
 
-sudo sed -i 's/\/\/      "o=Raspbian,n=buster";/      "o=Raspbian,n=buster";/ig' /etc/apt/apt.conf.d/50unattended-upgrades
-sudo sed -i 's/\/\/Unattended-Upgrade::Mail "root";/Unattended-Upgrade::Mail "pi";/ig' /etc/apt/apt.conf.d/50unattended-upgrades
+    sudo sed -i 's/\/\/      "o=Raspbian,n=$CODENAME";/      "o=Raspbian,n=$CODENAME";/ig' /etc/apt/apt.conf.d/50unattended-upgrades
+    sudo sed -i 's/\/\/Unattended-Upgrade::Mail "root";/Unattended-Upgrade::Mail "pi";/ig' /etc/apt/apt.conf.d/50unattended-upgrades
 
-sudo dpkg-reconfigure -plow unattended-upgrades -u
+    sudo dpkg-reconfigure -plow unattended-upgrades -u
 
-# Ask for mail
-OUTPUT="/tmp/input.txt"
-touch $OUTPUT
-trap 'rm "${OUTPUT}"; exit' SIGHUP SIGINT SIGTERM
+    # Ask for mail
+    OUTPUT="/tmp/input.txt"
+    touch $OUTPUT
+    trap 'rm "${OUTPUT}"; exit' SIGHUP SIGINT SIGTERM
 
-dialog --inputbox "Enter your e-mail:" 8 40 2>"${OUTPUT}"
-respose=$?
+    dialog --inputbox "Enter your E-mail:" 8 40 2>"${OUTPUT}"
+    respose=$?
 
-case $respose in
-0) sudo sed -i "s/email_address=root/email_address=$(<$OUTPUT)/ig" /etc/apt/listchanges.conf ;;
-1 | 255) echo "Cancelled." ;;
-esac
+    case $respose in
+    0) sudo sed -i "s/email_address=root/email_address=$(<$OUTPUT)/ig" /etc/apt/listchanges.conf ;;
+    1 | 255) echo "Cancelled." ;;
+    esac
 
-rm "$OUTPUT"
+    rm "$OUTPUT"
+    exit_message
+}
 
-read -p "Done!. Press [Enter] to continue..."
+install_script_message
+install
