6a7b87e579b70b538cd4d8165e51302029c20058
Jose Cerrejon <ulysess@gmail.com>
2021-12-30 16:33:13 +0100

    ❌ REMOVE: Scripts dwnmp3.sh, flexget.sh, hotstop.sh

--- scripts/inet/hotspot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Description : Install HotSpot thks to https://www.pihomeserver.fr
-# Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 0.1 (24/May/14)
-#
-# TODO        · https://www.pihomeserver.fr/en/2014/05/22/raspberry-pi-home-server-creer-hot-spot-wifi-captive-portal/
-#
-clear
