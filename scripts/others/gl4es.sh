2948e5229eede4805ce73bc64a8700a300ac2ce2
Jai A P <jai.jap.318@gmail.com>
2022-05-01 08:04:50 +0530

    Update gl4es.sh

--- scripts/others/gl4es.sh
+++ scripts/others/gl4es.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Description : PiKISS GUI from Jai-JAP
-# Author      : Jai A P (jai.jap.318@gmail.com)
-# Version     : 1.0.0 (16/Apr/22)
+# Description : GL4ES from ptitSeb
+# Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
+# Version     : 1.0.2 (03/Jan/21)
 # Compatible  : Raspberry Pi 1-4
 # Repository  : https://github.com/ptitSeb/gl4es
 #
@@ -10,13 +10,13 @@
 clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-readonly COMPILE_PATH="$HOME/sc/pikiss-gui"
-readonly PACKAGES_DEV=(ninja-build gobject-introspection libgee-0.8-dev libgirepository1.0-dev libgtk-3-dev valac libgnome-menu-3-dev)
-readonly GITHUB_PATH="https://github.com/Jai-JAP/pikiss-gui.git"
+readonly COMPILE_PATH="$HOME/sc/gl4es"
+readonly PACKAGES_DEV=(libx11-dev)
+readonly GITHUB_PATH="https://github.com/ptitSeb/gl4es.git"
 
 compile() {
     install_packages_if_missing "${PACKAGES_DEV[@]}"
-    pip_install meson
+
     mkdir -p "$HOME"/sc && cd "$_" || exit 1
     if [[ ! -d "$COMPILE_PATH" ]]; then
         echo -e "\nCloning and compiling repo...\n"
@@ -25,9 +25,12 @@ compile() {
         echo -e "\nDirectory already exists. Updating and compiling repo...\n"
         cd "$COMPILE_PATH" || exit 1
         git pull
+        rm -r build
     fi
-    ./build.sh
-    echo -e "\nDone!. You can run PiKISS GUI from Menu -> Accessories -> PiKISS GUI"
+    mkdir build && cd "$_" || exit 1
+    cmake .. -DBCMHOST=1 -DCMAKE_BUILD_TYPE=RELEASE
+    make_with_all_cores
+    echo -e "\nDone!. You can found the library at $COMPILE_PATH/lib"
     exit_message
 }
 
