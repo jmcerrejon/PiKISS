b3598a0ff6a7fcf35b3eebd5e27e01bbe190fb5c
jmcerrejon <ulysess@gmail.com>
2023-11-16 22:13:23 +0100

    👌 IMPROVE: Add support for Raspberry Pi OS Bookworm. Some minor changes

--- scripts/emus/gba.sh
+++ scripts/emus/gba.sh
@@ -2,8 +2,9 @@
 #
 # Description : Gameboy Advance emulator mgba
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.5.3 (28/Jan/23)
-# Compatible  : Raspberry Pi 4 (tested)
+# Contributor : Foxhound311
+# Version     : 1.6.0 (16/Nov/23)
+# Tested      : Raspberry Pi 5
 # Repository  : https://github.com/mgba-emu/mgba.git
 # Help        : 32 bits fix compilation at https://github.com/mgba-emu/mgba/issues/1081
 #
@@ -12,12 +13,14 @@ check_board || { echo "Missing file helper.sh. I've tried to download it for you
 clear
 
 readonly INSTALL_DIR="$HOME/games"
+readonly VERSION="0.11"
 readonly PACKAGES=(ffmpeg libglu1-mesa libzip4 qtmultimedia5-dev libqt5multimedia5 libqt5opengl5)
-readonly PACKAGES_DEV=(qtbase5-dev-tools qtbase5-dev cmake libsdl2-dev ffmpeg libnewlib-dev libzip-dev libedit-dev libsqlite3-dev libelf-dev libepoxy-dev libpng-dev libavcodec-dev libavresample-dev libminizip-dev libavfilter-dev qtmultimedia5-dev qt5-qmake qtdeclarative5-dev qttools5-dev zipcmp zipmerge ziptool)
-readonly SOURCE_CODE_URL="https://github.com/mgba-emu/mgba.git"
-readonly BINARY_URL="https://misapuntesde.com/rpi_share/mgba-0.11-rpi-aarch64.tar.gz"
-readonly BINARY_64_BITS_URL="https://misapuntesde.com/rpi_share/mgba-0.11-rpi-aarch64.tar.gz"
+readonly PACKAGES_DEV=(qtbase5-dev-tools qtbase5-dev cmake libsdl2-dev ffmpeg libnewlib-dev libzip-dev libedit-dev libsqlite3-dev libelf-dev libepoxy-dev libpng-dev libavcodec-dev libminizip-dev libavfilter-dev qtmultimedia5-dev qt5-qmake qtdeclarative5-dev qttools5-dev zipcmp zipmerge ziptool libjson-c-dev liblua5.3-dev)
+readonly SOURCE_CODE_URL="https://github.com/mgba-emu/mgba"
+readonly BINARY_URL="https://misapuntesde.com/rpi_share/mgba-${VERSION}-rpi-armhf.tar.gz"
+readonly BINARY_64_BITS_URL="https://misapuntesde.com/rpi_share/mgba-${VERSION}-rpi-aarch64.tar.gz"
 readonly GAME_URL="https://web.archive.org/web/20080625061600/http://www.freewebs.com/worldtreegames/TyrianGBA.zip"
+readonly WEREWOLF_ROM_URL="https://dlhb.gamebrew.org/gbahomebrews/awerewolftale.7z"
 
 runme() {
     if [ ! -d "$INSTALL_DIR"/mgba ]; then
@@ -26,7 +29,7 @@ runme() {
     fi
     echo
     read -p "Press [ENTER] to run..."
-    cd "$INSTALL_DIR"/mgba/bin && ./mgba-qt
+    cd "$INSTALL_DIR"/mgba && ./mgba-qt
     exit_message
 }
 
@@ -60,8 +63,8 @@ generate_icon() {
         cat <<EOF >~/.local/share/applications/mgba.desktop
 [Desktop Entry]
 Name=mGBA Game Boy Advance Emulator
-Exec=${INSTALL_DIR}/mgba/bin/mgba-qt
-Icon=${INSTALL_DIR}/mgba/share/icons/hicolor/128x128/apps/mgba.png
+Exec=${INSTALL_DIR}/mgba/mgba-qt
+Icon=${INSTALL_DIR}/mgba/share/icons/hicolor/128x128/apps/io.mgba.mGBA.png
 Type=Application
 Comment=mGBA is an emulator for running Game Boy Advance games. It aims to be faster and more accurate than many existing Game Boy Advance emulators
 Categories=Game;Emulator;
@@ -72,20 +75,21 @@ EOF
 compile() {
     install_packages_if_missing "${PACKAGES_DEV[@]}"
     mkdir -p "$HOME/sc" && cd "$_" || exit 1
-    git clone "$SOURCE_CODE_URL" mgba && cd "$_" || exit 1
+    git clone --recursive "$SOURCE_CODE_URL" mgba && cd "$_" || exit 1
     mkdir build && cd "$_" || exit 1
-    if is_kernel_64_bits; then
-        cmake -DCMAKE_INSTALL_PREFIX:PATH="$HOME/games/mgba" ..
-    else
-        cmake -DCMAKE_INSTALL_PREFIX:PATH="$HOME/games/mgba" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-march=armv8-a -mtune=cortex-a72" ..
-    fi
+    cmake -DCMAKE_INSTALL_PREFIX:PATH="$HOME/games/mgba" -DCMAKE_BUILD_TYPE=RelWithDebInfo .. # Add -DCMAKE_CXX_STANDARD_LIBRARIES=-latomic if you get an error about libatomic.so on 32 bits
+    echo -e "\n\nCompiling... Estimated time on RPi 5: ~ 5 min.\n"
     make_with_all_cores
     echo "Done!. Check $HOME/sc/mgba/build/qt/mgba-qt and $HOME/sc/mgba/build/sdl/mgba"
     exit_message
 }
 
 post_install() {
-    cp -ri "$INSTALL_DIR/mgba/share/.config" ~
+    echo -e "\nLinking libraries..."
+    ln -s "$INSTALL_DIR/mgba/lib/libmgba.so.0.11.0" "$INSTALL_DIR/mgba/lib/libmgba.so.0"
+    ln -s "$INSTALL_DIR/mgba/lib/libmgba.so.0.11.0" "$INSTALL_DIR/mgba/lib/libmgba.so.0.11"
+
+    download_and_extract "$INSTALL_DIR/mgba" "$INSTALL_DIR/mgba/share/mgba/roms"
 }
 
 install() {
@@ -96,7 +100,7 @@ install() {
     else
         download_and_extract "$BINARY_URL" "$INSTALL_DIR"
     fi
-    # post_install # TODO: Recover file from compressed file
+    post_install
     generate_icon
     echo -e "\nDone!. To play, go to Menu > Games > mGBA Game Boy Advance Emulator or $INSTALL_DIR/mgba/bin path and type: ./mgba ../roms/<rom_file>\n"
     runme
@@ -107,13 +111,12 @@ echo "
 Gameboy Advance Emulator (mgba)
 ===============================
 
- · Optimized for Raspberry Pi 4 | Thks Foxhound311 for the tips.
+ · Version ${VERSION} optimized for Raspberry Pi 5.
  · Install path: $INSTALL_DIR/mgba
- · GBA BIOS included.
  · bin/mgba-qt for the GUI frontend or bin/mgba for console.
- · SDL 2 and OpenGL 2.
- · More Info: https://github.com/mgba-emu/mgba
- · Homebrew ROMs included: Rick Dangerous (gba) and A Were Wolf Tale (gba).
+ · More Info: $SOURCE_CODE_URL
+ · GBA BIOS NOT included.
+ · Homebrew ROM included: A Were Wolf Tale (gba).
  · Keys: A: X | B: Z | L: A | R: S | Start: Enter | Select: Backspace | ALT+F4: Close
 "
 read -p "Press [ENTER] to continue..."
