e54247e661ac380fdd5411ed95ea2df87d2488e5
jmcerrejon <ulysess@gmail.com>
2024-10-01 10:32:27 +0200

    👌 IMPROVE: Bump version. Little improvements

--- scripts/emus/speccy.sh
+++ scripts/emus/speccy.sh
@@ -2,64 +2,101 @@
 #
 # Description : Portable ZX-Spectrum emulator
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.5.2 (17/Aug/20)
-# Compatible  : Raspberry 1-2 (¿?), Raspberry Pi 3-4 (tested)
+# Version     : 1.6.0 (01/Oct/24)
+# Tested      : Raspberry 5
 #
+# shellcheck source=../helper.sh
+. ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-INSTALL_DIR="/home/$USER/games/speccy"
-URL_FILE="https://misapuntesde.com/rpi_share/unreal_speccy_portable_0.0.86.11.tar.gz"
-URL_GAME="https://www.mojontwins.com/juegos/mojon-twins--ninjajar-eng-v1.1.tap"
+readonly INSTALL_DIR="$HOME/games"
+readonly PACKAGES=(libcogl20 libsdl2-2.0-0)
+readonly PACKAGES_DEV=(libcurl4-gnutls-dev libcogl20 libsdl2-dev)
+readonly URL_FILE="https://misapuntesde.com/rpi_share/unreal_speccy_portable_rpi.tar.gz"
+readonly WEBSITE_URL="https://bitbucket.org/djdron/unrealspeccyp"
 
-make_desktop_entry() {
-    if [[ ! -e /usr/share/applications/speccy.desktop ]]; then
-        sudo wget https://quantum-bits.org/tango/icons/computer-sinclair-zx-spectrum.png -O /usr/share/pixmaps/spectrum.png
-        sudo sh -c 'echo "[Desktop Entry]\nName=Speccy (ZX Spectrum)\nComment=Speccy emulates some versions of Sinclair ZX Spectrum.\nExec='"$INSTALL_DIR"'/unreal_speccy_portable\nIcon=/usr/share/pixmaps/spectrum.png\nTerminal=false\nType=Application\nCategories=Application;Game;\nPath='"$INSTALL_DIR"'/" > /usr/share/applications/speccy.desktop'
+run() {
+    if [[ -f "$INSTALL_DIR"/speccy/ninjajar.tap ]]; then
+        read -p "Do you want to play NinJAJAR? (y/N) " response
+        if [[ $response =~ [Nn] ]]; then
+            exit_message
+        fi
+        cd "$INSTALL_DIR"/speccy || exit 1
+        if [ "$(getconf LONG_BIT)" == "64" ]; then
+            ./usp.aarch64 ninjajar.tap
+        else
+            ./usp.armhf ninjajar.tap
+        fi
     fi
+    exit_message
 }
 
-validate_url() {
-    if [[ $(wget -S --spider "$1" 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then echo "true"; fi
+uninstall() {
+    echo
+    read -p "Do you want to uninstall Unreal Speccy Emulator (y/N)? " response
+    if [[ $response =~ [Yy] ]]; then
+        rm -rf ~/.local/share/applications/speccy.desktop ~/.usp
+        [[ -e $INSTALL_DIR/speccy ]] && sudo rm -rf "$INSTALL_DIR/speccy"
+        if [[ -e $INSTALL_DIR/speccy ]]; then
+            echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
+            exit_message
+        fi
+        echo -e "\nSuccessfully uninstalled."
+        exit_message
+    fi
+    exit_message
 }
 
-downloadROM() {
-    wget -qO "$INSTALL_DIR"/ninjajar.tap "$URL_GAME"
-}
+if [[ -e $INSTALL_DIR/speccy ]]; then
+    echo "Warning!: Unreal Speccy Emulator already installed."
+    uninstall
+fi
 
-playgame() {
-    if [[ -f "$INSTALL_DIR"/ninjajar.tap ]]; then
-        read -p "Do you want to play NinJaJar now? [y/n] " option
-        case "$option" in
-        y*) cd "$INSTALL_DIR" && ./unreal_speccy_portable ninjajar.tap ;;
-        esac
+generate_icon() {
+    if [[ ! -e ~/.local/share/applications/speccy.desktop ]]; then
+        echo "Creating shortcut...\n"
+        sudo wget https://quantum-bits.org/tango/icons/computer-sinclair-zx-spectrum.png -O /usr/share/pixmaps/spectrum.png
+        cat <<EOF >~/.local/share/applications/speccy.desktop
+[Desktop Entry]
+Name=Speccy (ZX Spectrum)
+Comment=Speccy emulates some versions of Sinclair ZX Spectrum.
+Exec=$INSTALL_DIR/speccy/run.sh
+Icon=/usr/share/pixmaps/spectrum.png
+Terminal=false
+Type=Application
+Categories=Application;Game;
+Path=$INSTALL_DIR/speccy
+EOF
     fi
 }
 
 install() {
     echo -e "\nInstalling, please wait...\n"
-    if [[ ! -f "$INSTALL_DIR"/unreal_speccy_portable ]]; then
-        mkdir -p "$HOME"/games && cd "$_"
-        wget -qO- -O speccy.tar.gz "$URL_FILE" && tar -xzvf speccy.tar.gz && rm speccy.tar.gz
-        cd speccy
-        mkdir roms # It's neccessary, even it's empty
-        downloadROM
-        make_desktop_entry
-    fi
-    echo -e "\nDone!. To play go to $INSTALL_DIR, copy any .tap file to directory and type: ./unreal_speccy_portable <game name>\n"
-    playgame
-    read -p "Press [Enter] to continue..."
-    exit
+    install_packages_if_missing "${PACKAGES[@]}"
+    download_and_extract "$URL_FILE" "$INSTALL_DIR"
+    generate_icon
+    echo -e "\nDone!. To play go to Menu > Games > Speccy or cd $INSTALL_DIR/speccy and type: ./run.sh\n"
+    run
 }
 
 compile_speccy() {
-    sudo apt install -y libcurl4-openssl-dev libcurl4-gnutls-dev libcogl-gles2-dev git cmake libsdl2-dev
-    cd ~ && git clone https://bitbucket.org/djdron/unrealspeccyp.git usp && cd usp/build/cmake
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_SDL=Off -DUSE_SDL2=On -DSDL2_INCLUDE_DIRS="/usr/inlude" -DCMAKE_CXX_FLAGS="$(sdl2-config --cflags)" -DCMAKE_EXE_LINKER_FLAGS="$(sdl2-config --libs)"
-    make -j"$(getconf _NPROCESSORS_ONLN)"
-    chmod +x unreal_speccy_portable
-    mkdir roms # It's neccessary, even it's empty
+    install_packages_if_missing "${PACKAGES_DEV[@]}"
+    mkdir -p "$HOME/sc" && cd "$_" || exit 1
+    git clone https://bitbucket.org/djdron/unrealspeccyp.git usp && cd usp/build/cmake || exit 1
+    cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_SDL=Off -DUSE_SDL2=On -DSDL2_INCLUDE_DIRS="/usr/inlude" -DCMAKE_CXX_FLAGS="$(sdl2-config --cflags)" -DCMAKE_EXE_LINKER_FLAGS="$(sdl2-config --libs)" -Wno-dev
+    make_with_all_cores
+    echo -e "\nDone!."
+    exit_message
 }
 
-echo -e "\nPortable ZX-Spectrum emulator (unrealspeccyp ver. 0.86.11)\n=========================================================\n\n\n· ESC to enter menu.\n· More Info: https://bitbucket.org/djdron/unrealspeccyp\n· Add Ninjajar by Mojon Twins.\n\nInstall path: $INSTALL_DIR\n"
+install_script_message
+echo "
+Portable ZX-Spectrum emulator (unrealspeccyp)
+=============================================
+
+ · ESC to enter menu
+ · Add Ninjajar by Mojon Twins.
+ · More info: $WEBSITE_URL"
 
 install
