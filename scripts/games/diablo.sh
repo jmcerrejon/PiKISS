7ff6fcaec72f10f2d93474b51be266bffa63b7fb
jmcerrejon <ulysess@gmail.com>
2025-03-29 10:21:10 +0100

    👌 IMPROVE: Add Diablo 1 support for aarch64

--- scripts/games/diablo.sh
+++ scripts/games/diablo.sh
@@ -2,19 +2,21 @@
 #
 # Description : Diablo for Raspberry Pi
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1.10 (11/Nov/21)
+# Version     : 1.2.0 (29/Mar/25)
 # Compatible  : Raspberry Pi 3-4 (tested)
 #
 # Help		  : https://github.com/diasurgical/devilutionX/
 #
-
-. ./scripts/helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+# shellcheck source=../helper.sh
+. ./scripts/helper.sh || . ../helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 readonly INSTALL_DIR="$HOME/games"
 readonly PACKAGES=(p7zip libsdl2-ttf-2.0-0 libsdl2-mixer-2.0-0)
-readonly BINARY_PATH="https://github.com/diasurgical/devilutionX/releases/download/1.3.0/devilutionx-linux-armhf.zip"
+readonly BINARY_URL="https://github.com/diasurgical/devilutionX/releases/download/1.3.0/devilutionx-linux-armhf.zip"
+readonly BINARY_64_BITS_URL="https://github.com/diasurgical/DevilutionX/releases/download/1.5.4/devilutionx-linux-aarch64.tar.xz"
+readonly SHAREWARE_URL="https://github.com/diasurgical/devilutionx-assets/releases/latest/download/spawn.mpq"
 readonly VAR_DATA_NAME="DIABLO_1"
 readonly DIABLO1_DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME")
 readonly ICON="https://misapuntesde.com/res/diablo1.png"
@@ -29,14 +31,10 @@ runme() {
     exit_message
 }
 
-remove_files() {
-    rm -rf "$INSTALL_DIR"/diablo1 ~/.local/share/applications/diablo1.desktop ~/.local/share/diasurgical
-}
-
 uninstall() {
     read -p "Do you want to uninstall Diablo 1 (y/N)? " response
     if [[ $response =~ [Yy] ]]; then
-        remove_files
+        rm -rf "$INSTALL_DIR"/diablo1 ~/.local/share/applications/diablo1.desktop ~/.local/share/diasurgical
         if [[ -e "$INSTALL_DIR"/diablo1 ]]; then
             echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
             exit_message
@@ -69,23 +67,52 @@ EOF
     fi
 }
 
-install() {
-    echo -e "\nInstalling Diablo 1, please wait..."
-    install_packages_if_missing "${PACKAGES[@]}"
+download_data_files() {
+    if exists_magic_file; then
+        echo -e "\nDownloading Data..."
+        DATA_URL=$(extract_path_from_file "$VAR_DATA_NAME")
+        message_magic_air_copy "DIABLO 1"
+        download_file "$DATA_URL" "$INSTALL_DIR"/diablo1
+        return 0
+    else
+        echo -e "\nDownloading shareware version..."
+        download_file "$SHAREWARE_URL" "$INSTALL_DIR"/diablo1
+        return 0
+    fi
+}
 
-    download_and_extract "$BINARY_PATH" "$INSTALL_DIR"
-    mv "$INSTALL_DIR"/devilutionx-linux-armhf "$INSTALL_DIR"/diablo1
-    generate_icon
-
-    if ! exists_magic_file; then
-        echo -e "\nNow copy diabdat.mpq (must be in lowercase) into $INSTALL_DIR/diablo1."
-        exit_message
+install_binaries() {
+    echo -e "\nInstalling, please wait..."
+    if is_userspace_64_bits; then
+        download_and_extract "$BINARY_64_BITS_URL" "$INSTALL_DIR/diablo1"
+    else
+        download_and_install "$BINARY_URL" "$INSTALL_DIR"
     fi
 
-    download_file "$DIABLO1_DATA_URL" "$INSTALL_DIR"/diablo1
-    echo -e "\nDone!. type $INSTALL_DIR/diablo1 to Play or go to Menu > Games > Diablo1 (if proceed).\n"
+    [[ -e "$INSTALL_DIR/devilutionx-linux-armhf" ]] && mv "$INSTALL_DIR/devilutionx-linux-armhf" "$INSTALL_DIR"/diablo1
+    [[ -e "$INSTALL_DIR/diablo1/devilutionx.deb" ]] && rm "$INSTALL_DIR/diablo1/devilutionx.deb"
+    [[ -e "$INSTALL_DIR/diablo1/devilutionx.rpm" ]] && rm "$INSTALL_DIR/diablo1/devilutionx.rpm"
+
+    return 0
+}
+
+install() {
+    install_packages_if_missing "${PACKAGES[@]}"
+
+    install_binaries
+    download_data_files
+    generate_icon
+
+    echo -e "\nDone! Type $INSTALL_DIR/diablo1 to play or go to Menu > Games > Diablo 1.\n"
     runme
 }
 
 install_script_message
+echo "
+Diablo 1
+========
+· The game needs the original game files to play it, but a shareware version will be installed.
+· Thanks to DevilutionX for the port.
+"
+
 install
