d2e92d62370f8ee7d0ff0ff8ccad22f9fdcd4937
jmcerrejon <ulysess@gmail.com>
2025-11-30 11:46:09 +0100

    👌 IMPROVE: JFDuke3D (Duke Nukem 3D) for aarch64 (replacing Eduke32)

--- scripts/games/duke3d.sh
+++ scripts/games/duke3d.sh
@@ -2,11 +2,11 @@
 #
 # Description : Duke Nukem 3D
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.1.2 (22/Nov/25)
-# Tested.     : Raspberry Pi 5
+# Version     : 1.1.2 (30/Nov/25)
+# Tested      : Raspberry Pi 5
 #
-# Help		  : https://github.com/nukeykt/NBlood <-- Better than Eduke32 official port for the Pi?
-# 			  : http://wiki.eduke32.com/wiki/Building_EDuke32_on_Linux#Prerequisites_for_the_build
+# Help		  : https://github.com/nukeykt/NBlood <-- Better than duke3d official port for the Pi?
+# 			  : http://wiki.duke3d.com/wiki/Building_duke3d_on_Linux#Prerequisites_for_the_build
 #
 # shellcheck source=../helper.sh
 . ./scripts/helper.sh || . ../helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
@@ -16,30 +16,27 @@ check_board || { echo "Missing file helper.sh. I've tried to download it for you
 
 readonly INSTALL_DIR="$HOME/games"
 readonly BINARY_URL="https://misapuntesde.com/rpi_share/eduke32.tar.gz"
-readonly BINARY_64_BITS_URL="https://misapuntesde.com/rpi_share/eduke32_arm64.tar.gz"
+readonly BINARY_64_BITS_URL="https://media.githubusercontent.com/media/jmcerrejon/pikiss-bin/refs/heads/main/games/duke3d-20240303-9-g87e7a5e-rpi-aarch64.tar.gz"
 readonly PACKAGES=(p7zip)
 readonly PACKAGES_DEV=(build-essential nasm libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev libsdl-mixer1.2-dev libsdl2-dev libsdl2-mixer-dev flac libflac-dev libvorbis-dev libvpx-dev libgtk2.0-dev freepats)
-readonly SOURCE_CODE_URL="https://voidpoint.io/terminx/eduke32.git"
+readonly SOURCE_CODE_URL="https://github.com/jonof/jfduke3d"
 readonly VAR_DATA_NAME="DUKE_ATOM"
 DATA_URL="http://hendricks266.duke4.net/files/3dduke13_data.7z"
-INPUT=/tmp/eduke32.$$
+INPUT=/tmp/duke3d.$$
+
 
 runme() {
     read -p "Press [ENTER] to run the game..."
-    cd "$INSTALL_DIR"/eduke32 && ./eduke32
+    cd "$INSTALL_DIR"/duke3d && ./duke3d
     echo
     exit_message
-}
-
-remove_files() {
-    rm -rf "$INSTALL_DIR"/eduke32 ~/.local/share/applications/eduke32.desktop
 }
 
 uninstall() {
     read -p "Do you want to uninstall Duke Nukem 3D (y/N)? " response
     if [[ $response =~ [Yy] ]]; then
-        remove_files
-        if [[ -e "$INSTALL_DIR"/eduke32 ]]; then
+        rm -rf "$INSTALL_DIR"/duke3d ~/.local/share/applications/duke3d.desktop
+        if [[ -e "$INSTALL_DIR"/duke3d ]]; then
             echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
             exit_message
         fi
@@ -49,38 +46,38 @@ uninstall() {
     exit_message
 }
 
-if [[ -d "$INSTALL_DIR"/eduke32 ]]; then
+if [[ -d "$INSTALL_DIR"/duke3d ]]; then
     echo -e "Duke Nukem 3D already installed.\n"
     uninstall
 fi
 
 generate_icon() {
     echo -e "\nGenerating icon..."
-    if [[ ! -e ~/.local/share/applications/eduke32.desktop ]]; then
-        cat <<EOF >~/.local/share/applications/eduke32.desktop
+    if [[ ! -e ~/.local/share/applications/duke3d.desktop ]]; then
+        cat <<EOF >~/.local/share/applications/duke3d.desktop
 [Desktop Entry]
 Name=Duke Nukem 3D
-Exec=${PWD}/eduke32/eduke32
-Icon=${PWD}/eduke32/icon.png
+Exec=${PWD}/duke3d/duke3d
+Icon=${PWD}/duke3d/icon.png
 Type=Application
 Comment=Duke Nukem 3D is fps game developed by 3D Realms in 1996.
 Categories=Game;ActionGame;
-Path=${PWD}/eduke32/
+Path=${PWD}/duke3d/
 EOF
     fi
 }
 
 download_data_files() {
-    cd "$INSTALL_DIR/eduke32" || exit 1
+    cd "$INSTALL_DIR/duke3d" || exit 1
     install_packages_if_missing "${PACKAGES[@]}"
-    download_and_extract "$DATA_URL" "$INSTALL_DIR"/eduke32
+    download_and_extract "$DATA_URL" "$INSTALL_DIR"/duke3d
 }
 
 compile() {
     echo -e "\nInstalling dependencies (if proceed)...\n"
     install_packages_if_missing "${PACKAGES_DEV[@]}"
     mkdir -p "$HOME"/sc && cd "$_" || exit 1
-    git clone "$SOURCE_CODE_URL" eduke32 && cd "$_" || exit 1
+    git clone "$SOURCE_CODE_URL" duke3d && cd "$_" || exit 1
     echo -e "\n\nCompiling... Estimated time on RPi 4: <10 min.\n"
     make_with_all_cores WITHOUT_GTK=1 USE_LIBVPX=0 HAVE_FLAC=0 RENDERTYPESDL=1 HAVE_JWZGLES=1 OPTLEVEL=0
     echo -e "\nDone.\n"
@@ -96,15 +93,8 @@ download_binaries() {
     download_and_extract "$INSTALL_URL" "$INSTALL_DIR"
 }
 
-ln_libflac_lib_on_32_bit() {
-    if [[ ! -f /usr/lib/arm-linux-gnueabihf/libFLAC.so.8 ]]; then
-        echo "Fixing libFLAC.so.8..."
-        ln -s /usr/lib/arm-linux-gnueabihf/libFLAC.so.12 /usr/lib/arm-linux-gnueabihf/libFLAC.so.8
-    fi
-}
-
 install() {
-    echo -e "\n\nInstalling EDuke32, please wait..."
+    echo -e "\n\nInstalling, please wait..."
     mkdir -p "$INSTALL_DIR" && cd "$_" || exit 1
     download_binaries
     generate_icon
@@ -113,12 +103,8 @@ install() {
         message_magic_air_copy "$VAR_DATA_NAME"
     fi
 
-    if ! is_userspace_64_bits; then
-        ln_libflac_lib_on_32_bit
-    fi
-
     download_data_files
-    echo -e "\nDone!. You can play typing $INSTALL_DIR/eduke32/eduke32 or opening the Menu > Games > Duke Nukem 3D.\n"
+    echo -e "\nDone!. You can play typing $INSTALL_DIR/duke3d/duke3d or opening the Menu > Games > Duke Nukem 3D.\n"
     runme
 }
 
@@ -127,12 +113,10 @@ echo "
 Duke Nukem 3D for Raspberry Pi
 ==============================
 
-· WARNING!! There is an issue with the resolution on Raspberry Pi. Try to set at 1280x720 or lower and don't try to change it on the game.
-
-· More Info: https://www.eduke32.com/
-· Install path: $INSTALL_DIR/eduke32
+· More Info: $SOURCE_CODE_URL
+· Install path: $INSTALL_DIR/duke3d
 · You need the game data files. Shareware version will be installed if you don't provide them.
-· If you want to play with the original game data files, copy them to $INSTALL_DIR/eduke32
+· If you want to play with the original game data files, copy them to $INSTALL_DIR/duke3d
 "
 read -p "Press [ENTER] to continue..."
 install
