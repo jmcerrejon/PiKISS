03683b3938c672573ab6ffe44f7f46dbf6f0fb2a
jmcerrejon <ulysess@gmail.com>
2025-12-20 09:38:57 +0100

    👌 IMPROVE: Games > OpenBOR v4.0 for aarch64

--- scripts/games/openbor.sh
+++ scripts/games/openbor.sh
@@ -2,40 +2,36 @@
 #
 # Description : OpenBOR
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.4.8 (07/Aug/23)
-# Compatible  : Raspberry Pi 4 (tested)
-# Repository  : https://github.com/crcerror/OpenBOR-63xx-RetroPie-openbeta
-# Help		  : https://www.raspberrypi.org/forums/viewtopic.php?f=78&t=26859&start=25
-#             : https://retropie.org.uk/forum/topic/19326/openbor-6xxx-openbeta-testphase
-#             : https://misapuntesde.com/post.php?id=567
-#             : https://drive.google.com/drive/u/0/folders/1NNqFuVfjyjcaS94q5YhSrNqumZ1r8LNi
+# Version     : 2.0.0 (20/Dec/25)
+# Tested      : Raspberry Pi 5
 #
-. ./scripts/helper.sh || . ../helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+# shellcheck source=../helper.sh
+. ../helper.sh || . ./scripts/helper.sh || . ../helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-INSTALL_DIR="$HOME/games"
-PACKAGES=(libsdl-gfx1.2-5 libpng16-16 libsdl2-gfx-1.0-0 libvorbisidec1)
-PACKAGES_DEV=(libsdl2-gfx-dev libvorbisidec-dev libvpx-dev libogg-dev libsdl2-gfx-1.0-0 libvorbisidec1)
-BINARY_URL="https://misapuntesde.com/rpi_share/openbor_by_ulysess.tar.gz"
-GITHUB_URL="https://github.com/crcerror/OpenBOR-Raspberry"
-DATA_URL="https://archive.org/download/sor-2-x-v-2.1/SOR2X_V2.1.pak"
+readonly INSTALL_DIR="$HOME/games"
+readonly PACKAGES=(libsdl-gfx1.2-5 libpng16-16 libsdl2-gfx-1.0-0 libvorbisidec1)
+readonly PACKAGES_DEV=(libsdl2-gfx-dev libvorbisidec-dev libvpx-dev libogg-dev libsdl2-gfx-1.0-0 libvorbisidec1)
+readonly BINARY_URL="https://misapuntesde.com/rpi_share/openbor-v4.0-rpi-aarch64.tar.gz
+"
+readonly SOURCE_CODE_URL="https://github.com/DCurrent/openbor"
 
 runme() {
+    if [ ! -f "$INSTALL_DIR"/openbor/openbor ]; then
+        echo -e "\nFile does not exist.\n· Something is wrong.\n· Try to install again."
+        exit_message
+    fi
     read -p "Press [ENTER] to run the game..."
     cd "$INSTALL_DIR"/openbor && ./openbor
     echo
     exit_message
 }
 
-remove_files() {
-    rm -rf "$INSTALL_DIR"/openbor ~/.local/share/applications/openbor.desktop
-}
-
 uninstall() {
     read -p "Do you want to uninstall Openbor (y/N)? " response
     if [[ $response =~ [Yy] ]]; then
-        remove_files
+        rm -rf "$INSTALL_DIR"/openbor ~/.local/share/applications/openbor.desktop
         if [[ -e "$INSTALL_DIR"/openbor ]]; then
             echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
             exit_message
@@ -67,24 +63,10 @@ EOF
     fi
 }
 
-path_libsdl_gfx() {
-    if [[ ! -e /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.13 ]]; then
-        echo -e "\nLinking libSDL_gfx..."
-        sudo ln -s /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.15 /usr/lib/arm-linux-gnueabihf/libSDL_gfx.so.13
-    fi
-}
-
-download_data_files() {
-    echo -e "\nDownloading Street Of Rage 2X, please wait..."
-    download_file "$DATA_URL" "$INSTALL_DIR"/openbor/Paks
-}
-
 install() {
     echo -e "\nInstalling Openbor, please wait..."
     install_packages_if_missing "${PACKAGES[@]}"
     download_and_extract "$BINARY_URL" "$INSTALL_DIR"
-    download_data_files
-    path_libsdl_gfx
     generate_icon
     echo -e "\nDone!."
     runme
@@ -94,22 +76,22 @@ compile() {
     install_packages_if_missing "${PACKAGES_DEV[@]}"
     mkdir -p ~/sc && cd "$_" || exit
     echo "Cloning and compiling repo..."
-    [[ ! -d ~/sc/openbor ]] && git clone "$GITHUB_URL"
+    [[ ! -d ~/sc/openbor ]] && git clone "$SOURCE_CODE_URL"
     make clean-all BUILD_PANDORA=1
     patch -p0 -i ./patch/latest_build.diff
     make_with_all_cores BUILD_PANDORA=1 PNDDEV=/usr
     echo -e "\nDone!"
 }
 
+install_script_message
 echo "
-OpenBOR for Raspberry Pi 4
-==========================
+OpenBOR for Raspberry Pi
+========================
 
- · Optimized for Raspberry Pi 4.
- · More Info and games: http://www.chronocrash.com/forum/
- · Game included: Streets Of Rage 2X thanks to Kratus http://www.chronocrash.com/forum/index.php?action=profile;u=13351
+ · More Info and games: https://www.chronocrash.com/forum/forums/modules.18 ¡ https://itch.io/games/tag-openbor
+ · Game included: Dungeons & Dragons by Zvitor https://www.zvitor.com/projeto/cave.html
  · Install path: $INSTALL_DIR/openbor
- · Some keys: F12 - Menu | 
+ · Some keys: F12 - Menu, ESC - Back, ENTER - Start, Arrow keys - Move, A - Attack, S - Jump
 "
 read -p "Press [ENTER] to continue..."
 
