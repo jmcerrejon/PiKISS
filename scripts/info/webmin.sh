6b2fdfa7e5e1410e27defe015c096ba4d8ddae8f
jmcerrejon <ulysess@gmail.com>
2026-04-25 11:44:08 +0200

    👌 IMPROVE: Info > Webmin

--- scripts/info/webmin.sh
+++ scripts/info/webmin.sh
@@ -2,29 +2,67 @@
 #
 # Description : Webmin
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
-# Version     : 1.0.0 (09/Jul/20)
-# Compatible  : Raspberry Pi 1-4 (tested)
+# Version     : 1.1.0 (25/Apr/26)
+# Tested      : Raspberry Pi 5
 #
-# HELP		  : https://msrobotics.net/index.php/com-virtuemart-menu-categories/laboratorio-pi/41-instala-webmin-y-administra-de-forma-grafica-raspbian-via-web
-#
+# shellcheck disable=SC1094
+# shellcheck disable=SC1091
+. ./scripts/helper.sh || . ../helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
 clear
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
-IP=$(hostname -I)
-WEBMIN_URL='https://netix.dl.sourceforge.net/project/webadmin/webmin/1.953/webmin_1.953_all.deb'
+IP=$(get_ip)
 
-download() {
-	cd "$HOME"
-	wget "$WEBMIN_URL"
+uninstall() {
+    read -p "Do you want to uninstall Webmin (y/N)? " response
+    if [[ $response =~ [Yy] ]]; then
+        [[ -e /etc/webmin/uninstall.sh ]] && sudo /etc/webmin/uninstall.sh
+        sudo apt-get remove --purge -y webmin
+        if [[ -e /usr/bin/webmin ]]; then
+            echo -e "I hate when this happens. I could not find the directory, Try to uninstall manually. Apologies."
+            exit_message
+        fi
+        echo -e "\nSuccessfully uninstalled."
+        exit_message
+    fi
+    exit_message
+}
+
+if [[ -e /usr/bin/webmin ]]; then
+    echo -e "Webmin already installed.\n"
+    uninstall
+    exit 1
+fi
+
+setup() {
+    curl -o /tmp/webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+    sudo sh /tmp/webmin-setup-repo.sh
+    rm /tmp/webmin-setup-repo.sh
 }
 
 install() {
 	echo -e "\nInstalling Webmin\n=================\n\nPlease wait..."
 	sudo apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions libapt-pkg-perl python
-	download
-	sudo dpkg -i webmin*.deb
-	rm webmin_*.deb
+	setup
+    sudo apt -y --fix-broken install
+    sudo apt-get install -y --install-recommends webmin usermin
+    echo -e "Done!. Now you can go to https://${IP}:10000 on your web browser. Some extra info:\n · FAQ: https://webmin.com/faq/\n · User and password: Any user in the system"
+    exit_message
 }
-install
 
-read -p "Done!. Now you can go to https://${IP}:10000 on your web browser. Some extra info:\n · FAQ: http://www.webmin.com/faq.html\n · The app is installed at /usr/share/webmin\n · User and password: Any user in the system (maybe pi with password raspberry?)"
-read -p "Press [ENTER] to go back to the menu..."
+install_script_message
+echo "
+Webmin
+======
+
+· Webmin is a web-based system administration tool.
+· Go to https://${IP}:10000 on your web browser when installed.
+· User and password: Any user in the system.
+"
+
+read -p "Do you want to install it? (y/N) " response
+if [[ $response =~ [Nn] ]]; then
+    exit_message
+fi
+
+install
